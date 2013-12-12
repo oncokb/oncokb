3133cf50e4168639adf9936f8649514d9a220026
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-12-12 16:16:46 -0500

    rename package

--- OncoGKB/src/main/java/org/mskcc/cbio/oncogkb/dao1/HibernateUtil.java
+++ OncoGKB/src/main/java/org/mskcc/cbio/oncogkb/dao1/HibernateUtil.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.mskcc.cbio.oncogkb.Dao;
+package org.mskcc.cbio.oncogkb.dao1;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
