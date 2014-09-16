ef24b8810b3b07296031938696a6a75528122f96
JJ Gao <jgao@cbio.mskcc.org>
2014-09-16 16:14:44 -0400

    level of evidence rewording

--- core/src/main/java/org/mskcc/cbio/oncokb/model/LevelOfEvidence.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/model/LevelOfEvidence.java
@@ -10,17 +10,10 @@ import java.util.Map;
  * @author jgao
  */
 public enum LevelOfEvidence {
-    LEVEL_1 ("1", "FDA-approved biomarker in approved indication. The alteration is"
-            + " used or has been used as an eligibility criterion for clinical"
-            + " trials of tis agent or class of agents."),
-    LEVEL_2 ("2", "FDA-approved biomarker in unapproved indication. There is clinical"
-            + " evidence for an association between this biomarker and response/resistance"
-            + " to this agent or class of agents in another tumor type ONLY."),
-    LEVEL_3 ("3", "Clinical evidence linking unapproved biomarker to response."
-            + " There is limited clinical evidence (early or conflicting data)"
-            + " for an association between this alteration and response/resistance"
-            + " to this agent or class of agents."),
-    LEVEL_4 ("4", "Preclinical evidence linking unapproved biomarker to response.");
+    LEVEL_1 ("1", "FDA-approved	biomarker in approved indication"),
+    LEVEL_2 ("2", "FDA-approved	biomarker in unapproved indication"),
+    LEVEL_3 ("3", "Clinical evidence linking unapproved biomarker to response"),
+    LEVEL_4 ("4", "Preclinical evidence linking unapproved biomarker to response");
     
     private LevelOfEvidence(String level, String description) {
         this.level = level;
