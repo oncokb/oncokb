4feb544ab6e22092151a6126525b7a15e0f2547b
Hongxin Zhang <jackson.zhang.828@gmail.com>
2014-10-28 12:32:44 -0400

    Add summary; Add resistant to; Add evidence level explanation;

--- web/src/main/webapp/app/scripts/controllers/VariantCtrl.js
+++ web/src/main/webapp/app/scripts/controllers/VariantCtrl.js
@@ -24,6 +24,10 @@ angular.module('webappApp')
             $scope.isCollapsed = {};
 
             $scope.displayParts = {
+                annotationSummary: {
+                    displayName: 'Summary',
+                    objectName: 'annotation_summary'
+                },
                 geneAnnotation: {
                     displayName: 'Gene Annotation',
                     objectName: 'gene_annotation'
@@ -244,7 +248,7 @@ angular.module('webappApp')
         $scope.generateReport = function() {
             $scope.generaingReport =true;
             var params = generateReportData();
-            
+
             // $timeout(function() {
             //     $scope.generaingReport =false;
             // }, 1000)
@@ -262,24 +266,24 @@ angular.module('webappApp')
         
         function generateReportData() {
             var params = {
-                "patientName": "LNAME322",
+                "patientName": "",
                 "specimen": "",
                 "clientNum": "",
                 "overallInterpretation": "",
-                "diagnosis": "LUNG CANCER",
-                "tumorTissueType": "LUNG",
+                "diagnosis": "",
+                "tumorTissueType": "",
                 "specimenSource": "",
                 "blockId": "",
                 "stage": "",
                 "grade": "",
-                "geneName": "BRAF",
-                "mutation": "V600E",
-                "alterType": "IN-FRAME DELETION",
-                "mutationFreq": "23%",
+                "geneName": "",
+                "mutation": "",
+                "alterType": "",
+                "mutationFreq": "",
                 "tumorTypeDrugs": "",
                 "nonTumorTypeDrugs": "",
                 "hasClinicalTrial": "",
-                "treatment": "[{ 'NCCN Guidelines': {Disease: Non-Small Cell}]",
+                "treatment": "",
                 "fdaApprovedInTumor": "",
                 "fdaApprovedInOtherTumor": "",
                 "clinicalTrials": "",
@@ -289,13 +293,15 @@ angular.module('webappApp')
 
             // console.log($scope.annotation);
 
-            params.overallInterpretation = $scope.annotation.annotation_summary || '';
-            params.gneeName = $scope.geneName;
+            params.overallInterpretation = ($scope.geneName + ' SUMMARY\n' + 
+                $scope.annotation.annotation_summary + 
+                '\nOTHER GENES\nNo additional somatic mutations were detected in this patient sample in the other sequenced gene regions.') || '';
+            params.geneName = $scope.geneName;
             params.mutation = $scope.mutation;
             params.alterType = $scope.selectedTumorType.name;
             
             if($scope.relevantCancerType) {
-                var _treatment = constructfdaInfo();
+                var _treatment = constructTreatment();
                 params.treatment = _treatment.length > 0 ? _treatment : "";
                 var _fdaInfo = constructfdaInfo();
                 params.fdaApprovedInTumor = _fdaInfo.approved.length > 0 ? _fdaInfo.approved : "";
@@ -349,9 +355,10 @@ angular.module('webappApp')
                 cancerTypeInfo = $scope.relevantCancerType;
 
             if($scope.annotation.annotation_summary) {
-                key = $scope.geneName + "SUMMARY";
+                key = $scope.geneName + " SUMMARY";
                 value.push({'description': $scope.annotation.annotation_summary});
                 object[key] = value;
+                treatment.push(object);
             }
 
             if(cancerTypeInfo.nccn_guidelines) {
@@ -394,10 +401,15 @@ angular.module('webappApp')
             return treatment;
         }
 
-        function findApprovedDrug(datum, object, tumorType) {
+        function findApprovedDrug(datum, object, tumorType, key) {
             for(var m=0, datumL = datum.length; m < datumL; m++) {
                 var _subDatum = datum[m],
                     _key = '';
+
+                if(typeof key !== 'undefined') {
+                    _key = key;
+                }
+
                 if(_subDatum.treatment) {
                     for (var i = 0; i < _subDatum.treatment.length; i++) {
                         var _treatment = _subDatum.treatment[i];
@@ -423,10 +435,15 @@ angular.module('webappApp')
             return object;
         }
 
-        function findByLevelEvidence(datum, object, tumorType) {
+        function findByLevelEvidence(datum, object, tumorType, key) {
             for(var m=0, datumL = datum.length; m < datumL; m++) {
                 var _subDatum = datum[m],
                     _key = '';
+
+                if(typeof key !== 'undefined') {
+                    _key = key;
+                }
+
                 if(_subDatum.level_of_evidence_for_patient_indication 
                     && _subDatum.level_of_evidence_for_patient_indication.level
                     && Number(_subDatum.level_of_evidence_for_patient_indication.level) < 4) {
@@ -445,7 +462,14 @@ angular.module('webappApp')
 
                     _key = _key.substr(0, _key.length-3);
 
-                    object[_key] = {'description': _subDatum.description};
+                    if(object.hasOwnProperty(_key)) {
+                        console.log('key duplicated: ' + _key);
+                    }else {
+                        object[_key] = {
+                            'level of evidence': _subDatum.level_of_evidence_for_patient_indication.level,
+                            'description': _subDatum.description
+                        };
+                    }
                 }
             }
             return object;
@@ -455,19 +479,28 @@ angular.module('webappApp')
             var fdaApproved = [],
                 fdaNonApproved = [],
                 object = {},
-                cancerTypeInfo = $scope.relevantCancerType;
+                cancerTypeInfo = $scope.relevantCancerType,
+                attrsToDisplay = ['sensitive_to', 'resistant_to'];
 
             if(cancerTypeInfo.standard_therapeutic_implications) {
-                var _datum = cancerTypeInfo.standard_therapeutic_implications.sensitive_to;
-                object = {};
-                    
-                object = findApprovedDrug(_datum, object);
+                for (var i = 0; i < attrsToDisplay.length; i++) {
+                    if(cancerTypeInfo.standard_therapeutic_implications[attrsToDisplay[i]]) {
+                        var _datum = cancerTypeInfo.standard_therapeutic_implications[attrsToDisplay[i]];
+                        
+                        object = {};
+                        if(attrsToDisplay[i] === 'sensitive_to') {
+                            object = findApprovedDrug(_datum, object);
+                        }else {
+                            object = findApprovedDrug(_datum, object, '', $scope.displayProcess(attrsToDisplay[i]) + ': ');
+                        }
 
-                for(var _key in object) {
-                    var _object = {};
-                    _object[_key] = object[_key]
-                    fdaApproved.push(_object);
-                    _object = null;
+                        for(var _key in object) {
+                            var _object = {};
+                            _object[_key] = object[_key]
+                            fdaApproved.push(_object);
+                            _object = null;
+                        }
+                    }
                 }
             }
 
@@ -476,8 +509,16 @@ angular.module('webappApp')
             for (var i = 0; i < $scope.annotation.cancer_type.length; i++) {
                 if($scope.annotation.cancer_type[i].type !== $scope.relevantCancerType.type) {
                     if($scope.annotation.cancer_type[i].standard_therapeutic_implications) {
-                        var _datum = $scope.annotation.cancer_type[i].standard_therapeutic_implications.sensitive_to;
-                        object = findApprovedDrug(_datum, object, $scope.annotation.cancer_type[i].type);
+                        for (var j = 0; j < attrsToDisplay.length; j++) {
+                            if($scope.annotation.cancer_type[i].standard_therapeutic_implications[attrsToDisplay[j]]) {
+                                var _datum = $scope.annotation.cancer_type[i].standard_therapeutic_implications[attrsToDisplay[j]];
+                                if(attrsToDisplay[j] === 'sensitive_to') {
+                                    object = findApprovedDrug(_datum, object, $scope.annotation.cancer_type[i].type);
+                                }else {
+                                    object = findApprovedDrug(_datum, object, $scope.annotation.cancer_type[i].type, $scope.displayProcess(attrsToDisplay[j]) + ': ');
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -496,7 +537,8 @@ angular.module('webappApp')
                 key = '',
                 value = [],
                 object = {},
-                cancerTypeInfo = $scope.relevantCancerType;
+                cancerTypeInfo = $scope.relevantCancerType,
+                attrsToDisplay = ['sensitive_to', 'resistant_to'];
 
             if(cancerTypeInfo.clinical_trial) {
                 var _datum = cancerTypeInfo.clinical_trial;
@@ -521,6 +563,7 @@ angular.module('webappApp')
             }
 
             if(cancerTypeInfo.investigational_therapeutic_implications && cancerTypeInfo.investigational_therapeutic_implications.general_statement) {
+                var hasdrugs = false;
                 value = [];
                 object = {};
                 key = "INVESTIGATIONAL THERAPEUTIC IMPLICATIONS";
@@ -528,17 +571,40 @@ angular.module('webappApp')
                 object[key] = value;
                 clincialTrials.push(object);
 
-                if(cancerTypeInfo.investigational_therapeutic_implications.sensitive_to) {
-                    object = {};
-                    object = findByLevelEvidence(cancerTypeInfo.investigational_therapeutic_implications.sensitive_to, object);
-                    
-                    for(var _key in object) {
-                        var _object = {};
-                        _object[_key] = object[_key]
-                        clincialTrials.push(_object);
-                        _object = null;
+                for (var j = 0; j < attrsToDisplay.length; j++) {
+                    if(cancerTypeInfo.investigational_therapeutic_implications[attrsToDisplay[j]]) {
+                        object = {};
+                        if(attrsToDisplay[j] === 'sensitive_to') {
+                            object = findByLevelEvidence(cancerTypeInfo.investigational_therapeutic_implications[attrsToDisplay[j]], object);
+                        }else {
+                            object = findByLevelEvidence(cancerTypeInfo.investigational_therapeutic_implications[attrsToDisplay[j]], object, '', $scope.displayProcess(attrsToDisplay[j]) + ': ');
+                        }
+                        if(Object.keys(object).length) {
+                            hasdrugs = true;
+                        }
+                        for(var _key in object) {
+                            var _object = {};
+                            _object[_key] = object[_key]
+                            clincialTrials.push(_object);
+                            _object = null;
+                        }
                     }
                 }
+
+                if(!hasdrugs) {
+                    clincialTrials.push({'no_evidence': 'There are no investigational therapies that meet level 1, 2 or 3 evidence.'});
+                }
+
+                object = {};
+                key = 'LEVEL OF EVIDENCE';
+                value =  [
+                    {'level 1': 'This alteration has been used as an eligibility criterion for clinical trials of this agent or class of agents.'},
+                    {'level 2': 'There is clinical evidence for an association between this biomarker and response/resistance to this agent of class of agents in another tumor type only.'},
+                    {'level 3': 'There is limited clinical evidence (early or conflicting data) for an association between this alteration and response/resistance to this agent or class of agents.'},
+                    {'level 4': 'There is preclinical evidence linking unapproved biomarker to response.'},
+                ];
+                object[key] = value;
+                clincialTrials.push(object);
             }
             return clincialTrials;
         }
