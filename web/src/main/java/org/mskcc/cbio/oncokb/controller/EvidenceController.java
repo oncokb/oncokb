2944a0fa9a9777cf1d20d4ebade93eaa2d0f2367
Hongxin Zhang <jackson.zhang.828@gmail.com>
2014-11-06 13:20:41 -0500

    Add evidenceType and alteration support in EvidenceController

--- web/src/main/java/org/mskcc/cbio/oncokb/controller/EvidenceController.java
+++ web/src/main/java/org/mskcc/cbio/oncokb/controller/EvidenceController.java
@@ -5,11 +5,16 @@
 package org.mskcc.cbio.oncokb.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import org.mskcc.cbio.oncokb.bo.EvidenceBo;
 import org.mskcc.cbio.oncokb.bo.GeneBo;
+import org.mskcc.cbio.oncokb.bo.AlterationBo;
+import org.mskcc.cbio.oncokb.model.Alteration;
+import org.mskcc.cbio.oncokb.model.AlterationType;
 import org.mskcc.cbio.oncokb.model.Evidence;
+import org.mskcc.cbio.oncokb.model.EvidenceType;
 import org.mskcc.cbio.oncokb.model.Gene;
 import org.mskcc.cbio.oncokb.util.ApplicationContextSingleton;
 
@@ -28,7 +33,9 @@ public class EvidenceController {
     @RequestMapping(value="/evidence.json")
     public @ResponseBody List<Evidence> getEvidence(
             @RequestParam(value="entrezGeneId", required=false) Integer entrezGeneId,
-            @RequestParam(value="hugoSymbol", required=false) String hugoSymbol) {
+            @RequestParam(value="hugoSymbol", required=false) String hugoSymbol,
+            @RequestParam(value="alteration", required=false) String alt,
+            @RequestParam(value="type", required=false) String evidenceType) {
         
         GeneBo geneBo = ApplicationContextSingleton.getGeneBo();
         
@@ -39,12 +46,41 @@ public class EvidenceController {
             gene = geneBo.findGeneByHugoSymbol(hugoSymbol);
         }
         
+        AlterationBo alterationBo = ApplicationContextSingleton.getAlterationBo();
+        List<Alteration> alteration = new ArrayList<Alteration>();
+        if (alt != null && gene != null) {
+            Alteration alterationDatum = alterationBo.findAlteration(gene, AlterationType.MUTATION, alt);
+            if(alterationDatum != null) {
+                alteration.add(alterationDatum);
+            }
+        }
         EvidenceBo evidenceBo = ApplicationContextSingleton.getEvidenceBo();
         
-        if (gene == null) {
-            return evidenceBo.findAll();
+        EvidenceType type = null;
+        if(evidenceType != null) {
+            EvidenceType[] types = EvidenceType.values();
+            for (EvidenceType type1 : types) {
+                if (evidenceType.toLowerCase().equals(type1.label().toLowerCase())) {
+                    type = type1;
+                    break;
+                }
+            }
         }
         
-        return evidenceBo.findEvidencesByGene(gene);
+        if (gene != null && !alteration.isEmpty()) {
+            if(type != null) {
+                return evidenceBo.findEvidencesByAlteration(alteration, type);
+            }else {
+                return evidenceBo.findEvidencesByAlteration(alteration);     
+            }       
+        }else if(gene != null && alteration.isEmpty()) {
+            if(type != null) {
+                return evidenceBo.findEvidencesByGene(gene, type);
+            }else {
+                return evidenceBo.findEvidencesByGene(gene);
+            }
+        }else {
+            return evidenceBo.findAll();
+        }
     }
 }
