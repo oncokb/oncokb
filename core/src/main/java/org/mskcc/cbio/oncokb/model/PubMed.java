c5e12cfc2eae7a7ce66acd6d92bb6eb059658520
Hongxin Zhang <jackson.zhang.828@gmail.com>
2015-03-09 15:17:59 -0400

    Added OncokbInfo servlet, add getPubMedInfo and merged getSuggestions, getUsers functions

---
+++ core/src/main/java/org/mskcc/cbio/oncokb/model/PubMed.java
@@ -0,0 +1,26 @@
+package org.mskcc.cbio.oncokb.model;
+
+/**
+ *
+ * @author jgao
+ */
+public class PubMed {
+    private String gene;
+    private String links;
+
+    public String getGene() {
+        return gene;
+    }
+
+    public void setGene(String gene) {
+        this.gene = gene;
+    }
+
+    public String getLinks() {
+        return links;
+    }
+
+    public void setLinks(String links) {
+        this.links = links;
+    }
+}
