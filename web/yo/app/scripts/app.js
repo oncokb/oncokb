e69c86f43f9d682d9092da24a36bde1edb5e010f
Hongxin Zhang <jackson.zhang.828@gmail.com>
2015-04-24 11:56:46 -0400

    Added num-html sorting plug-in for dataTables and apply it into data summary tables

--- web/yo/app/scripts/app.js
+++ web/yo/app/scripts/app.js
@@ -535,3 +535,18 @@ gapi.load('auth:client:drive-share:drive-realtime', function () {
         angular.bootstrap(document, ['oncokbApp']);
     });
 });
+
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "num-html-pre": function ( a ) {
+        var x = jQuery(a).text();
+        return parseFloat( x );
+    },
+
+    "num-html-asc": function ( a, b ) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+
+    "num-html-desc": function ( a, b ) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+} );
