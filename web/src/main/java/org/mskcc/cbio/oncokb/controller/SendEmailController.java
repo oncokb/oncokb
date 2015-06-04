5baa252ed3a6382629de9eb72a061bb0d01d720b
Hongxin Zhang <jackson.zhang.828@gmail.com>
2015-06-04 17:04:28 -0400

    Add google username / password to sending emails; Remove unusable email fucnctions

--- web/src/main/java/org/mskcc/cbio/oncokb/controller/SendEmailController.java
+++ web/src/main/java/org/mskcc/cbio/oncokb/controller/SendEmailController.java
@@ -14,7 +14,7 @@ import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.security.GeneralSecurityException;
-        import java.util.Properties;
+import java.util.Properties;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
@@ -31,7 +31,7 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
 
-        import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,7 +48,7 @@ public class SendEmailController {
             @RequestParam(value="subject", required=false) String subject,
             @RequestParam(value="content", required=false) String body,
             @RequestParam(value="sendTo", required=false) String sendTo) throws IOException, MessagingException, GeneralSecurityException, ServiceException {
-        
+
         if(subject != null && body != null) {
             String propFileName = "properties/config.properties";
             Properties prop = new Properties();
@@ -63,22 +63,29 @@ public class SendEmailController {
 
             String from = prop.getProperty("google.username");
             String pass = prop.getProperty("google.password");
+            String toP = prop.getProperty("curation.log.email.to");
+
             String[] to = new String[1]; // list of recipient email addresses
 
             if(sendTo != null) {
                 to[0] = sendTo;
-            }else{
-                to[0] = "oncokb.curation@gmail.com";
+            }else if(toP != null && !toP.isEmpty()){
+                to[0] = toP;
             }
-//            MimeMessage email =  createEmail(to[0], from, subject, body);
-//            sendMessage(GoogleAuth.getGmailService(), "me", email);
+
+            if(!to[0].isEmpty()){
+                sendFromGMail(from, pass, to, subject, body);
+            }else{
+                return false;
+            }
+
             sendFromGMail(from, pass, to, subject, body);
             return true;
         }else {
             return false;
         }
     }
-    
+
     private static Boolean sendFromGMail(String from, String pass, String[] to, String subject, String body) {
         Properties props = System.getProperties();
         String host = "smtp.gmail.com";
@@ -119,46 +126,9 @@ public class SendEmailController {
         catch (MessagingException me) {
             me.printStackTrace();
         }
-        
+
         return false;
     }
-    
-    /**
-    * Send an email from the user's mailbox to its recipient.
-    *
-    * @param service Authorized Gmail API instance.
-    * @param userId User's email address. The special value "me"
-    * can be used to indicate the authenticated user.
-    * @param email Email to be sent.
-    * @throws MessagingException
-    * @throws IOException
-    */
-//    private static void sendMessage(Gmail service, String userId, MimeMessage email)
-//        throws MessagingException, IOException {
-//      Message message = createMessageWithEmail(email);
-//      message = service.users().messages().send(userId, message).execute();
-//
-//      System.out.println("Message id: " + message.getId());
-//      System.out.println(message.toPrettyString());
-//    }
-
-    /**
-     * Create a Message from an email
-     *
-     * @param email Email to be set to raw of message
-     * @return Message containing base64url encoded email.
-     * @throws IOException
-     * @throws MessagingException
-     */
-//    private static Message createMessageWithEmail(MimeMessage email)
-//        throws MessagingException, IOException {
-//      ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-//      email.writeTo(bytes);
-//      String encodedEmail = Base64.encodeBase64URLSafeString(bytes.toByteArray());
-//      Message message = new Message();
-//      message.setRaw(encodedEmail);
-//      return message;
-//    }
 
     /**
      * Create a MimeMessage using the parameters provided.
@@ -171,68 +141,19 @@ public class SendEmailController {
      * @throws MessagingException
      */
     private static MimeMessage createEmail(String to, String from, String subject,
-        String bodyText) throws MessagingException {
-      Properties props = new Properties();
-      Session session = Session.getDefaultInstance(props, null);
+                                           String bodyText) throws MessagingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
 
-      MimeMessage email = new MimeMessage(session);
-      InternetAddress tAddress = new InternetAddress(to);
-      InternetAddress fAddress = new InternetAddress(from);
+        MimeMessage email = new MimeMessage(session);
+        InternetAddress tAddress = new InternetAddress(to);
+        InternetAddress fAddress = new InternetAddress(from);
 
-      email.setFrom(new InternetAddress(from));
-      email.addRecipient(javax.mail.Message.RecipientType.TO,
-                         new InternetAddress(to));
-      email.setSubject(subject);
-      email.setText(bodyText);
-      return email;
-    }
-
-    /**
-     * Create a MimeMessage using the parameters provided.
-     *
-     * @param to Email address of the receiver.
-     * @param from Email address of the sender, the mailbox account.
-     * @param subject Subject of the email.
-     * @param bodyText Body text of the email.
-     * @param fileDir Path to the directory containing attachment.
-     * @param filename Name of file to be attached.
-     * @return MimeMessage to be used to send email.
-     * @throws MessagingException
-     */
-    private static MimeMessage createEmailWithAttachment(String to, String from, String subject,
-        String bodyText, String fileDir, String filename) throws MessagingException, IOException {
-      Properties props = new Properties();
-      Session session = Session.getDefaultInstance(props, null);
-
-      MimeMessage email = new MimeMessage(session);
-      InternetAddress tAddress = new InternetAddress(to);
-      InternetAddress fAddress = new InternetAddress(from);
-
-      email.setFrom(fAddress);
-      email.addRecipient(javax.mail.Message.RecipientType.TO, tAddress);
-      email.setSubject(subject);
-
-      MimeBodyPart mimeBodyPart = new MimeBodyPart();
-      mimeBodyPart.setContent(bodyText, "text/plain");
-      mimeBodyPart.setHeader("Content-Type", "text/plain; charset=\"UTF-8\"");
-
-      Multipart multipart = new MimeMultipart();
-      multipart.addBodyPart(mimeBodyPart);
-
-      mimeBodyPart = new MimeBodyPart();
-      DataSource source = new FileDataSource(fileDir + filename);
-
-      mimeBodyPart.setDataHandler(new DataHandler(source));
-      mimeBodyPart.setFileName(filename);
-      String contentType = Files.probeContentType(FileSystems.getDefault()
-          .getPath(fileDir, filename));
-      mimeBodyPart.setHeader("Content-Type", contentType + "; name=\"" + filename + "\"");
-      mimeBodyPart.setHeader("Content-Transfer-Encoding", "base64");
-
-      multipart.addBodyPart(mimeBodyPart);
-
-      email.setContent(multipart);
-
-      return email;
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(bodyText);
+        return email;
     }
 }
