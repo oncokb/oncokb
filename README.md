dfcc67c4ce6ee731981c2fdef154229947fdc2d4
Hongxin Zhang <jackson.zhang.828@gmail.com>
2015-06-06 18:42:48 -0400

    Change README; Add LICENSE;

--- README.md
+++ README.md
@@ -2,3 +2,77 @@ Repository for OncoKB, an oncogenomics knowledgebase.
 
 Multi-tier system for annotating and classifying mutation and copy number alteration events in cancer.
 
+We use Google Reatime API to store all information curators generated.
+We use MySQL to store data after reviewing
+
+#Front-end
+OncoKB front-end is built with lots of great open source JS libraries. AngularJS is used to be framework. Bower is used to manage denpendencies. Yeoman is used to initial project, angular-generator is used to create angular directive/service/factory etc. 
+
+##Install project
+1. Install npm & bower & yo (globally)
+2. Go to web/yo folder
+3. npm install
+4. bower install
+    * Choose 2.  es5-shim#>=3.4.0
+    * Choose 1.  angular#1.2.28
+
+##Use website without back-end
+1. Copy data-EXAMPLE to data under web/yo/app/scripts/
+2. Set parameter 'dataFromFile' to true in web/yo/app/scripts/services/dataconnector.js
+3. Under web/yo/, run 'grunt serve'
+
+## Show error in console
+Uncomment the $delegate
+`````````
+$provide.decorator('$exceptionHandler', function($delegate, $injector){
+    return function(exception, cause){
+        var $rootScope = $injector.get('$rootScope');
+        $rootScope.addError({message: 'Exception', reason: exception, case: cause});
+        //$delegate(exception, cause);
+    };
+});
+`````````
+
+## app.js file setting (Will be moved to front end configure file)
+```
+OncoKB.config = {
+    clientId: 'Your client ID from google developer console',
+    scopes: [
+        'https://www.googleapis.com/auth/plus.profile.emails.read',
+        'https://www.googleapis.com/auth/drive.file'
+    ],
+    folderId: '', //The folder ID where you put all google realtime documents
+    backupFolderId: '', //The backup folder ID
+    userRoles: {
+        'public': 1, // 0001
+        'user':   2, // 0010
+        'curator':4, // 0100
+        'admin':  8  // 1000
+    },
+    users: '', //Your user management Google Spreadsheet ID
+    accessLevels: {}
+};
+```
+
+#Properties file
+1. database.properties
+    * jdbc.driverClassName : We use mysql as database. Here it will be com.mysql.jdbc.Driver
+    * jdbc.url: Databse : url
+    * jdbc.username & jdbc.password: MySQL user name and password
+2. config.properties
+    * google.p_twelve : Your P12 private key path (You can generate this file from google developer console, more detials in Wiki)
+    * google.service_account_email : Your service account email from google developer console.
+    * google.username & google.password(Optional) : Google account info. They are used to send email
+    * curation.log.email.to(Optional) : The email address where all emails will go
+
+
+License
+--------------------
+
+OncoKB free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+A public instance of OncoKB (http://oncokb.org) is hosted and maintained by Memorial Sloan Kettering Cancer Center. It provides access to all curators in MSKCC knowledgebase team.
+
+If you are interested in coordinating the development of new features, please contact oncokb@cbio.mskcc.org.
--- web/yo/app/scripts/controllers/gene.js
+++ web/yo/app/scripts/controllers/gene.js
@@ -103,12 +103,7 @@ angular.module('oncokbApp')
 
             $scope.saveAllGenes = function() {
                 $scope.status.saveAllGenes = false;
-//              console.log($scope.documents);
                 saveGene($scope.documents, 0, function(){ $scope.status.saveAllGenes = true;});
-
-//              var sampleGenes = _.slice($scope.documents, 0, 50);
-//              saveGene(sampleGenes, 0);
-//              saveGene(Documents.get({title: 'U2AF1'}), 0);
             };
 
             $scope.userRole = users.getMe().role;
@@ -140,18 +135,8 @@ angular.module('oncokbApp')
                 rendering: true
             };
             $scope.getDocs();
-            // var newGenes = ['RET','FGFR4','KIT','SMO','ALK1','ERBB2','PIK3CA','PIK3R1','PTEN','DDR2','FGFR3','IDH1','KRAS','MAP2K1','MET','NOTCH1','NRAS','AKT1','FBXW7','FGFR2','FOXL2','GNA11','GNAQ','GNAS','HRAS','PDGFRB','PTCH1','STK11','CTNNB1','ERBB4','IDH2','RAD21','TET1','TET2','GATA1','GATA2','MPL','B2M','BTK','HLA-A','PRDM1','SF3B1','EPHA7','HIST1H1C','REL','CREBBP','VHL','PBRM1','SETD2','AKT2','AKT3','ERBB3','FGFR1','BRIP1','ERCC4','FANCA','FANCC','EWSR1','MDM2','MDM4','ETV6','DNMT1','EZH2','WT1','EP300','MYD88','CARD11','CD79B','KEAP1','MYC','FH','SDHA','SDHAF2','SDHB','SDHC','SDHD','RASA1','FOXA1','AURKA','AURKB','CCND1','CCNE1','PPP2R1A','CCND2','CCND3','PAK1','ERG','ETV1','SPOP','SOX2','MYOD1','CTCF','MTOR','PDGFRA','DAXX','MLH1','MSH6','RAD54L','RECQL4','BCL2','RB1','EIF1AX','EIF4A2','EIF4E','EPCAM','FAT1','SMAD2','SMAD3','SMAD4','TGFBR1','TGFBR2','U2AF1','BMPR1A','XPO1','ATRX','SMARCB1','CD276','CD274','CTLA4','TNFRSF14','IL7R','JAK1','JAK2','JAK3','IKZF1','ACVR1','IGF1','IGF1R','IGF2','INHA','INHBA','BAP1','NF2','RAF1','CDKN1B','RHOA','RAC1','MEN1','CDH1','STAG2','MDC1','MRE11A','POLD1','SOX9','ZFHX3','NF1','RAD50','RAD51B','RAD51C','RAD51D','RAD52','TOP1','HIST1H3A','KDM5C','KDM6A','KMT2A','KMT2C','KMT2D','H3F3A','H3F3B','H3F3C','KDM5A','POLE','DNMT3A','DNMT3B','AR','E2F3','FOXP1','RYBP','SHQ1','PAX8','TCEB1','CDKN2A','SMARCA4','TERT','BRCA1','BRCA2','PALB2','RHEB','TSC1','TSC2','RICTOR','ARID1A','ERCC2','CIC','FUBP1','HNF1A','MED12','BCOR','YAP1','LATS1','LATS2','MST1','ESR1','ATM','CHEK1','CHEK2','ATR','CENPA','MITF','FLT3','CEBPA','NPM1','RBM10','APC','ARAF','ARID1B','ARID2','ARID5B','BCL2L11','BRD4','CASP8','CBL','CDK12','CDK4','CDK6','CDKN1A','CDKN2B','GATA3','KDR','MAP2K2','MAP2K4','MAP3K1','MAX','MSH2','MYCN','NFE2L2','NFKBIA','NTRK1','NTRK2','NTRK3','PIK3CB','PIK3R2','PMS2','PTPN11','PTPRD','ROS1','RUNX1','SRC','TMPRSS2','XRCC2'];
-            // var newGenes = ['ERBB2','PIK3CA','PIK3R1','PTEN','DDR2','FGFR3','IDH1','KRAS','MAP2K1','MET','NOTCH1','NRAS','AKT1','FBXW7','FGFR2','FOXL2','GNA11','GNAQ','GNAS','HRAS','PDGFRB','PTCH1','STK11','CTNNB1','ERBB4','IDH2'];
-            var newGenes = ['GLI1', 'ETV4'];
 
-            //phase 1/2 genes 210
-            // var newGenes = ['RAD21','MSH2','XRCC2','TET1','TET2','GATA1','GATA2','MPL','CBL','KDR','PTPN11','B2M','BTK','HLA-A','PRDM1','SF3B1','EPHA7','HIST1H1C','REL','CREBBP','VHL','PBRM1','SETD2','AKT2','AKT3','ERBB3','FGFR1','BRIP1','ERCC4','FANCA','FANCC','EWSR1','MDM2','MDM4','ETV6','DNMT1','EZH2','WT1','ROS1','EP300','MYD88','CARD11','CD79B','KEAP1','NFE2L2','MYC','FH','SDHA','SDHAF2','BRD4','MAX','MYCN','RASA1','MAP2K2','MAP2K4','MAP3K1','FOXA1','AURKA','AURKB','CCND1','CCNE1','PPP2R1A','CCND2','CCND3','PAK1','ERG','ETV1','SPOP','TMPRSS2','SOX2','KMT2C','MYOD1','SRC','CTCF','CDKN1A','CDKN2B','APC','NTRK1','NTRK2','NTRK3','CDK4','CDK6','NFKBIA','MTOR','PDGFRA','DAXX','MLH1','MSH6','RAD54L','RECQL4','PMS2','BCL2L11','BCL2','RB1','EIF1AX','EIF4A2','EIF4E','GATA3','EPCAM','FAT1','SMAD2','SMAD3','SMAD4','TGFBR1','TGFBR2','U2AF1','BMPR1A','XPO1','ATRX','SMARCB1','CD276','CD274','CTLA4','TNFRSF14','IL7R','JAK1','JAK2','JAK3','IKZF1','ACVR1','IGF1','IGF1R','IGF2','INHA','INHBA','BAP1','NF2','RAF1','CDKN1B','RHOA','RAC1','MEN1','ARAF','CDH1','STAG2','MDC1','MRE11A','POLD1','SOX9','ZFHX3','NF1','CASP8','RAD50','RAD51B','RAD51C','RAD51D','RAD52','TOP1','HIST1H3A','KDM5C','KDM6A','KMT2A','KMT2D','H3F3A','H3F3B','H3F3C','KDM5A','POLE','PTPRD','DNMT3A','DNMT3B','RUNX1','AR','E2F3','FOXP1','RYBP','SHQ1','PIK3CB','PIK3R2','SDHB','SDHC','SDHD','PAX8','TCEB1','CDKN2A','SMARCA4','TERT','BRCA1','BRCA2','PALB2','CDK12','RHEB','TSC1','TSC2','RICTOR','ARID1A','ERCC2','ARID1B','ARID2','ARID5B','CIC','FUBP1','HNF1A','MED12','BCOR','YAP1','LATS1','LATS2','MST1','ESR1','ATM','CHEK1','CHEK2','ATR','CENPA','MITF','FLT3','CEBPA','NPM1','RBM10'];
-            // first 50 genes
-            // var newGenes = ['RAD21','MSH2','XRCC2','TET1','TET2','GATA1','GATA2','MPL','CBL','KDR','PTPN11','B2M','BTK','HLA-A','PRDM1','SF3B1','EPHA7','HIST1H1C','REL','CREBBP','VHL','PBRM1','SETD2','AKT2','AKT3','ERBB3','FGFR1','BRIP1','ERCC4','FANCA','FANCC','EWSR1','MDM2','MDM4','ETV6','DNMT1','EZH2','WT1','ROS1','EP300','MYD88','CARD11','CD79B','KEAP1','NFE2L2','MYC','FH','SDHA','SDHAF2','BRD4'];
-            // Second 50 genes
-            // var newGenes = ['MAX','MYCN','RASA1','MAP2K2','MAP2K4','MAP3K1','FOXA1','AURKA','AURKB','CCND1','CCNE1','PPP2R1A','CCND2','CCND3','PAK1','ERG','ETV1','SPOP','TMPRSS2','SOX2','KMT2C','MYOD1','SRC','CTCF','CDKN1A','CDKN2B','APC','NTRK1','NTRK2','NTRK3','CDK4','CDK6','NFKBIA','MTOR','PDGFRA','DAXX','MLH1','MSH6','RAD54L','RECQL4','PMS2','BCL2L11','BCL2','RB1','EIF1AX','EIF4A2','EIF4E','GATA3','EPCAM','FAT1'];
-            // rest 110 genes
-            // var newGenes = ['SMAD2','SMAD3','SMAD4','TGFBR1','TGFBR2','U2AF1','BMPR1A','XPO1','ATRX','SMARCB1','CD276','CD274','CTLA4','TNFRSF14','IL7R','JAK1','JAK2','JAK3','IKZF1','ACVR1','IGF1','IGF1R','IGF2','INHA','INHBA','BAP1','NF2','RAF1','CDKN1B','RHOA','RAC1','MEN1','ARAF','CDH1','STAG2','MDC1','MRE11A','POLD1','SOX9','ZFHX3','NF1','CASP8','RAD50','RAD51B','RAD51C','RAD51D','RAD52','TOP1','HIST1H3A','KDM5C','KDM6A','KMT2A','KMT2D','H3F3A','H3F3B','H3F3C','KDM5A','POLE','PTPRD','DNMT3A','DNMT3B','RUNX1','AR','E2F3','FOXP1','RYBP','SHQ1','PIK3CB','PIK3R2','SDHB','SDHC','SDHD','PAX8','TCEB1','CDKN2A','SMARCA4','TERT','BRCA1','BRCA2','PALB2','CDK12','RHEB','TSC1','TSC2','RICTOR','ARID1A','ERCC2','ARID1B','ARID2','ARID5B','CIC','FUBP1','HNF1A','MED12','BCOR','YAP1','LATS1','LATS2','MST1','ESR1','ATM','CHEK1','CHEK2','ATR','CENPA','MITF','FLT3','CEBPA','NPM1','RBM10'];
+            var newGenes = [];
 
             $scope.migrate = function(){
                 //console.log($scope.documents);
@@ -165,16 +150,6 @@ angular.module('oncokbApp')
                             $scope.status.migrate = true;
                         }
                     });
-                //Steo 0. Create a folder to put all files (Manully did)
-                //var newFolderId = '0BzBfo69g8fP6fjVqN1dCbGdYRUYxd0xfck5FTjdWLXVCaEZxZjhuWTFIcFBVODk3ZDNEX0E';
-                //Step 1. For loop all documents
-                //Step 2. Create file with same name
-                //Step 3. Give file same permission
-                //Step 4. Copy file content, but create
-
-                //storage.getPermission($scope.documents[0].id).then(function(result){
-                //    console.log(result);
-                //});
             };
 
             $scope.create = function() {
