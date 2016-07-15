1ead312ba72bd01ebf337fbd643cceb8e9ef0e96
Hongxin Zhang <jackson.zhang.828@gmail.com>
2016-07-15 12:51:20 -0400

    Update all level script with PMID comparison and Absent case in old list

--- core/src/main/java/org/mskcc/cbio/oncokb/scripts/AlterationLevel.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/scripts/AlterationLevel.java
@@ -27,9 +27,11 @@ public class AlterationLevel {
 
     public static void main(String[] args) throws IOException {
         List<Gene> genes = ApplicationContextSingleton.getGeneBo().findAll();
-        List<Map<String, String>> allLevels = getRecords();
+//        List<Gene> genes = ApplicationContextSingleton.getGeneBo().findGenesByHugoSymbol(Collections.singleton("ALK"));
+        List<Map<String, Object>> allLevels = getRecords();
+        List<Map<String, Object>> newList = new ArrayList<>();
 
-        System.out.println("Gene\tAlteration\tCancer Type\tSubtype\tLevel\tTreatment\tExist");
+        System.out.println("Gene\tAlteration\tTumor Type\tLevel\tTreatment\tPMIDs\tExist");
         for (Gene gene : genes) {
             List<Alteration> alterations = ApplicationContextSingleton.getAlterationBo().findAlterationsByGene(Collections.singleton(gene));
             Set<Alteration> alterationsWithoutVUS = AlterationUtils.excludeVUS(new HashSet<>(alterations));
@@ -57,11 +59,12 @@ public class AlterationLevel {
                         levelStr = level.getLevel().toUpperCase();
                     }
 
-                    if (oncoTreeType != null && (levelStr.equals("1") || levelStr.equals("2A"))) {
+                    if (oncoTreeType != null && (levelStr.equals("1") || levelStr.equals("2A") || levelStr.equals("3A"))) {
                         String cancerTypeStr = oncoTreeType.getCancerType();
                         String subtypeStr = oncoTreeType.getSubtype();
                         Set<Treatment> treatments = evidence.getTreatments();
                         List<String> treatmentNames = new ArrayList<>();
+                        Set<String> pmids = new HashSet<>();
 
                         for (Treatment treatment : treatments) {
                             Set<Drug> drugs = treatment.getDrugs();
@@ -76,40 +79,81 @@ public class AlterationLevel {
 
                             treatmentNames.add(drugStr);
                         }
+                        
+                        for(Article article : evidence.getArticles()) {
+                            pmids.add(article.getPmid());
+                        }
 
                         String treatmentStr = StringUtils.join(treatmentNames, ", ");
 
                         if (treatmentStr != null && !treatmentStr.equals("")) {
-                            Map<String, String> query = new HashMap<>();
+                            Map<String, Object> query = new HashMap<>();
                             query.put("gene", gene.getHugoSymbol());
                             query.put("alteration", alteration.getAlteration());
                             query.put("level", levelStr);
                             query.put("tumorType", subtypeStr == null ? cancerTypeStr : subtypeStr);
                             query.put("treatment", treatmentStr);
-                            System.out.println(gene.getHugoSymbol() + "\t" + alteration.getAlteration() + "\t" + cancerTypeStr + "\t" + subtypeStr + "\t" + levelStr + "\t" + treatmentStr + "\t" + hasMatch(allLevels, query));
+                            query.put("pmids", pmids);
+                            newList.add(query);
+                            System.out.println(gene.getHugoSymbol() + "\t" + alteration.getAlteration() + "\t" + (subtypeStr == null ? cancerTypeStr : subtypeStr) + "\t" + levelStr + "\t" + treatmentStr + "\t" + sortPMIDs(StringUtils.join(pmids, ", ")) + "\t" + hasMatch(allLevels, query));
                         }
                     }
                 }
+            }
+        }
 
-//                String level = "NA";
-//
-//                if (levelOfEvidence != null && levelOfEvidence.getLevel() != null) {
-//                    level = levelOfEvidence.getLevel().toUpperCase();
-//                }
+        System.out.println("Comparing with old list to identify anything not in the new list");
+        
+        //Don't compare pmids
+        for(Map<String, Object> query : newList) {
+            query.remove("pmids");
+        }
+        for(Map<String, Object> record : allLevels) {
+            record.remove("pmids");
+            if(!newList.contains(record)) {
+                System.out.println(record.get("gene") + "\t" + record.get("alteration") + "\t" + record.get("level") + "\t" + record.get("tumorType") + "\t" + record.get("treatment"));
             }
         }
     }
 
-    public static Boolean hasMatch(List<Map<String, String>> records, Map<String, String> query) {
+    public static Boolean hasMatch(List<Map<String, Object>> records, Map<String, Object> query) {
         Boolean hasMatch = false;
 
-        if (records.contains(query)) {
-            hasMatch = true;
+        
+        for(Map<String, Object> record : records) {
+            if(sameRecord(record, query)){
+                hasMatch = true;
+                break;
+            }
         }
         return hasMatch;
     }
 
-    public static List<Map<String, String>> getRecords() {
+    private static Boolean sameRecord(Map<String, Object>record, Map<String, Object>query) {
+        Boolean hasMatch = true;
+        if(!record.get("gene").equals(query.get("gene"))) {
+            hasMatch = false;
+        }
+        if(!record.get("alteration").equals(query.get("alteration"))) {
+            hasMatch = false;
+        }
+        if(!record.get("level").equals(query.get("level"))) {
+            hasMatch = false;
+        }
+        if(!record.get("tumorType").equals(query.get("tumorType"))) {
+            hasMatch = false;
+        }
+        if(!record.get("treatment").equals(query.get("treatment"))) {
+            hasMatch = false;
+        }
+        Set<String> recordpmids = (Set<String>) record.get("pmids");
+        Set<String> querypmids = (Set<String>) query.get("pmids");
+        if(!querypmids.containsAll(recordpmids)) {
+            hasMatch = false;
+        }
+        return hasMatch;
+    }
+    public static List<Map<String, Object>> getRecords() {
         try {
             String allLevels = PropertiesUtils.getProperties("google.all_levels_final");
             URL SPREADSHEET_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full/" + allLevels);
@@ -119,17 +163,18 @@ public class AlterationLevel {
             WorksheetFeed worksheetFeed = service.getFeed(
                 spreadSheetEntry.getWorksheetFeedUrl(), WorksheetFeed.class);
             List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
-            URL url = worksheets.get(2).getListFeedUrl();
+            URL url = worksheets.get(4).getListFeedUrl();
             ListFeed list = service.getFeed(url, ListFeed.class);
-            List<Map<String, String>> records = new ArrayList<>();
+            List<Map<String, Object>> records = new ArrayList<>();
 
             for (ListEntry row : list.getEntries()) {
-                Map<String, String> record = new HashMap<>();
+                Map<String, Object> record = new HashMap<>();
                 record.put("gene", row.getCustomElements().getValue("gene"));
                 record.put("alteration", row.getCustomElements().getValue("alteration"));
                 record.put("level", row.getCustomElements().getValue("level"));
                 record.put("tumorType", row.getCustomElements().getValue("tumortype"));
                 record.put("treatment", row.getCustomElements().getValue("treatment"));
+                record.put("pmids", convertToSetPmids(row.getCustomElements().getValue("pmids")));
                 records.add(record);
             }
             return records;
@@ -141,5 +186,23 @@ public class AlterationLevel {
             e.printStackTrace();
         }
         return null;
+    }
+    
+    private static Set<String> convertToSetPmids(String pmids) {
+        Set<String> pmidList = new HashSet<>();
+        String[] list = StringUtils.split(pmids,", ");
+
+        for(String pmid : list) {
+            pmidList.add(StringUtils.trim(pmid));
+        }
+        return pmidList;
+    }
+    private static String sortPMIDs(String pmids) {
+        Set<String> pmidList = convertToSetPmids(pmids);
+        
+        String[] newList = pmidList.toArray(new String[pmidList.size()]);
+        Arrays.sort(newList);
+        
+        return StringUtils.join(newList, ", ");
     }
 }
