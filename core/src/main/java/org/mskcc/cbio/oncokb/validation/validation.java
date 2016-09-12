93f9343fa7cd954fe2385a86ccb921fa6af6a7d0
Hongxin <zhx828@users.noreply.github.com>
2016-09-12 18:20:42 -0400

    Add a new rule to detect multiple mutation effects; Fix importer bug (#147)
    * Save mutation effect if any property of it is present
    
    Previously we only store mutation effect if the mutation effect
    checkbox is selected
    
    * Add rule for multiple mutation effects in one variant.
    
    Relevant variants are not counted
    
    * Validator bugs fixed

--- core/src/main/java/org/mskcc/cbio/oncokb/validation/validation.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/validation/validation.java
@@ -5,24 +5,16 @@
  */
 package org.mskcc.cbio.oncokb.validation;
 
+import org.mskcc.cbio.oncokb.model.*;
+import org.mskcc.cbio.oncokb.util.AlterationUtils;
+import org.mskcc.cbio.oncokb.util.EvidenceUtils;
+import org.mskcc.cbio.oncokb.util.GeneUtils;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import org.mskcc.cbio.oncokb.model.Alteration;
-import org.mskcc.cbio.oncokb.model.Evidence;
-import org.mskcc.cbio.oncokb.model.Gene;
-import org.mskcc.cbio.oncokb.model.MutationEffect;
-import org.mskcc.cbio.oncokb.model.Oncogenicity;
-import org.mskcc.cbio.oncokb.util.AlterationUtils;
-import org.mskcc.cbio.oncokb.util.EvidenceUtils;
-import org.mskcc.cbio.oncokb.util.GeneUtils;
+import java.util.*;
 
 /**
  *
@@ -34,8 +26,8 @@ public class validation {
         Map<Gene, Set<Evidence>> allGeneBasedEvidences = EvidenceUtils.getAllGeneBasedEvidences();
         Set<Gene> genes = GeneUtils.getAllGenes();
         Integer count = 0;
-        String result0 = "", result1 = "", result2 = "", result3 = "", result4 = "";
-        Integer length0 = 0, length1 = 0, length2 = 0, length3 = 0, length4 = 0;
+        String result0 = "", result1 = "", result2 = "", result3 = "", result4 = "", result5 = "";
+        Integer length0 = 0, length1 = 0, length2 = 0, length3 = 0, length4 = 0, length5 = 0;
         ArrayList<String> specialAlterations = new ArrayList<>(Arrays.asList("Inactivating Mutations", "Activating Mutations", "Fusions", "Inactivating", "Wildtype", "Amplification", "Fusions"));
         for (Gene gene : genes) {
             Set<Evidence> evidences = allGeneBasedEvidences.get(gene);
@@ -43,6 +35,7 @@ public class validation {
             Map<Alteration, ArrayList<Alteration>> relevantAlterationsMapping = new HashMap<Alteration, ArrayList<Alteration>>();
             Map<Alteration, String> oncogenicityMapping = new HashMap<Alteration, String>();
             Map<Alteration, String> mutationEffectMapping = new HashMap<Alteration, String>();
+            Map<Alteration, Set<Evidence>> multipleMutationEffects = new HashMap<>();
             Map<Alteration, Set<String>> referencesMapping = new HashMap<Alteration, Set<String>>();
             ArrayList<Alteration> altsWithDescriptions = new ArrayList<Alteration>();
             Set<Alteration> allVariants = new HashSet<Alteration>();
@@ -57,6 +50,12 @@ public class validation {
                     }
                     if (evidenceItem.getEvidenceType().toString().equals("MUTATION_EFFECT")) {
                         mutationEffectMapping.put(alterationItem, evidenceItem.getKnownEffect());
+                    }
+                    if (evidenceItem.getEvidenceType().toString().equals("MUTATION_EFFECT")) {
+                        if (!multipleMutationEffects.containsKey(alterationItem)) {
+                            multipleMutationEffects.put(alterationItem, new HashSet<Evidence>());
+                        }
+                        multipleMutationEffects.get(alterationItem).add(evidenceItem);
                     }
                     if(referencesMapping.containsKey(alterationItem)){
                         Set<String> oldPMIDs = referencesMapping.get(alterationItem);
@@ -83,8 +82,12 @@ public class validation {
                     }
                 }
                 for (Alteration relevantAlt : relevantAlts) {
-                    if (mutationEffectMapping.containsKey(alt) && mutationEffectMapping.containsKey(relevantAlt) && !mutationEffectMapping.get(alt).equals(mutationEffectMapping.get(relevantAlt))) {
-                        result1 += alt.toString() + ": " + mutationEffectMapping.get(alt) + ". Relevant alteration: " + relevantAlt.toString() + ": " + mutationEffectMapping.get(relevantAlt) +  "\n";
+                    if (mutationEffectMapping.containsKey(alt) && mutationEffectMapping.containsKey(relevantAlt) &&
+                        mutationEffectMapping.get(alt) != null && mutationEffectMapping.get(relevantAlt) != null &&
+                        !mutationEffectMapping.get(alt).equals(mutationEffectMapping.get(relevantAlt))) {
+                        result1 += alt.toString() + ": " + mutationEffectMapping.get(alt) +
+                            ". Relevant alteration: " + relevantAlt.toString() + ": " +
+                            mutationEffectMapping.get(relevantAlt) +  "\n";
                         length1++;
                         break;
                     }
@@ -110,6 +113,10 @@ public class validation {
                     result4 += alt.toString() + "\n";
                     length4++;
                 }
+                if (multipleMutationEffects.containsKey(alt) && multipleMutationEffects.get(alt).size() > 1) {
+                    result5 += alt.toString() + "\n";
+                    length5++;
+                }
 
             }
             count++;
@@ -133,6 +140,10 @@ public class validation {
         output += "******************************************************************************************\n";
         output += "Rule 5 check result: There are " + Integer.toString(length4) + " variants that have oncogenic and mutation effect, but don't have any PMIDs.\n";
         output += result4;
+
+        output += "******************************************************************************************\n";
+        output += "Rule 6 check result: There are " + Integer.toString(length5) + " variants that have multiple mutation effects (does not count relevant mutation effects)\n";
+        output += result5;
 
         try {
             File file = new File("validation-result.txt");
