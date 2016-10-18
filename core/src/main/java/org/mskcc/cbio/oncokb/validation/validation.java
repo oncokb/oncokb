f6295327c873d55dc7cb1eb5bb964adde383c8d7
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2016-10-18 16:00:30 -0400

    save to google spreadsheet instead of txt file

--- core/src/main/java/org/mskcc/cbio/oncokb/validation/validation.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/validation/validation.java
@@ -5,29 +5,76 @@
  */
 package org.mskcc.cbio.oncokb.validation;
 
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.ParentReference;
+import com.google.gdata.client.spreadsheet.SpreadsheetService;
+import com.google.gdata.data.spreadsheet.ListEntry;
+import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
+import com.google.gdata.data.spreadsheet.WorksheetEntry;
+import com.google.gdata.data.spreadsheet.WorksheetFeed;
+import com.google.gdata.util.ServiceException;
 import org.mskcc.cbio.oncokb.model.*;
 import org.mskcc.cbio.oncokb.util.AlterationUtils;
 import org.mskcc.cbio.oncokb.util.EvidenceUtils;
 import org.mskcc.cbio.oncokb.util.GeneUtils;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.GeneralSecurityException;
 import java.util.*;
+import org.mskcc.cbio.oncokb.util.GoogleAuth;
 
 /**
  *
  * @author jiaojiao
  */
 public class validation {
+    private static final String REPORT_PARENT_FOLDER = "0BzBfo69g8fP6fkNjY0RSQlNtRUgyRHVNeWJ5ZzFkU2twVGtYR19Bb1dld0JZY0VPd3hCTms";
+    private static final String REPORT_DATA_TEMPLATE = "1yjcti-clYq61RKAPcXD33lutcjCW82FLwk8KaPntO7A";
+    public static void main(String[] args) throws IOException, GeneralSecurityException, URISyntaxException, ServiceException {
+        Drive driveService = GoogleAuth.getDriveService();
+        System.out.println("Got drive service");
+        String fileName = "Validator Report";            
+        com.google.api.services.drive.model.File file = new com.google.api.services.drive.model.File();
+        file.setTitle(fileName);
+        file.setParents(Arrays.asList(new ParentReference().setId(REPORT_PARENT_FOLDER)));
+        file.setDescription("New File created from server");
 
-    public static void main(String[] args) throws IOException {
+        System.out.println("Copying file");
+
+        file = driveService.files().copy(REPORT_DATA_TEMPLATE, file).execute();
+
+        System.out.println("Successfully copied file. Start to change file content");
+        
+        String fileId = file.getId();
+        URL SPREADSHEET_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full/" + fileId);
+
+        SpreadsheetService service = GoogleAuth.getSpreadSheetService();
+        SpreadsheetEntry spreadSheetEntry = service.getEntry(SPREADSHEET_FEED_URL, SpreadsheetEntry.class);
+
+        WorksheetFeed worksheetFeed = service.getFeed(
+        spreadSheetEntry.getWorksheetFeedUrl(), WorksheetFeed.class);
+        List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
+        WorksheetEntry worksheet1 = worksheets.get(1), 
+                worksheet2 = worksheets.get(2), 
+                worksheet3 = worksheets.get(3), 
+                worksheet4 = worksheets.get(4), 
+                worksheet5 = worksheets.get(5), 
+                worksheet6 = worksheets.get(6);
+        
+        // Fetch the list feed of the worksheets.
+        URL listFeedUrl1 = worksheet1.getListFeedUrl(), 
+            listFeedUrl2 = worksheet2.getListFeedUrl(), 
+            listFeedUrl3 = worksheet3.getListFeedUrl(), 
+            listFeedUrl4 = worksheet4.getListFeedUrl(), 
+            listFeedUrl5 = worksheet5.getListFeedUrl(), 
+            listFeedUrl6 = worksheet6.getListFeedUrl();
+        
+
         Map<Gene, Set<Evidence>> allGeneBasedEvidences = EvidenceUtils.getAllGeneBasedEvidences();
         Set<Gene> genes = GeneUtils.getAllGenes();
         Integer count = 0;
-        String result0 = "", result1 = "", result2 = "", result3 = "", result4 = "", result5 = "";
-        Integer length0 = 0, length1 = 0, length2 = 0, length3 = 0, length4 = 0, length5 = 0;
         ArrayList<String> specialAlterations = new ArrayList<>(Arrays.asList("Inactivating Mutations", "Activating Mutations", "Fusions", "Inactivating", "Wildtype", "Amplification", "Fusions"));
         for (Gene gene : genes) {
             Set<Evidence> evidences = allGeneBasedEvidences.get(gene);
@@ -80,8 +127,13 @@ public class validation {
                         && oncogenicityMapping.get(relevantAlt) != null
                         && oncogenicityMapping.containsKey(relevantAlt) 
                         && !oncogenicityMapping.get(alt).equals(oncogenicityMapping.get(relevantAlt))) {
-                        result0 += alt.toString() + ": " + Oncogenicity.getByLevel(oncogenicityMapping.get(alt)).getDescription() + ". Relevant alteration: " +  relevantAlt.toString() + ": " + Oncogenicity.getByLevel(oncogenicityMapping.get(relevantAlt)).getDescription() + "\n";
-                        length0++;
+                        ListEntry row = new ListEntry();
+                        row.getCustomElements().setValueLocal("Gene", alt.getGene().getHugoSymbol());
+                        row.getCustomElements().setValueLocal("Alteration", alt.getAlteration());
+                        row.getCustomElements().setValueLocal("Oncogenicty", Oncogenicity.getByLevel(oncogenicityMapping.get(alt)).getDescription() );
+                        row.getCustomElements().setValueLocal("RelevantAlteration", relevantAlt.getAlteration());
+                        row.getCustomElements().setValueLocal("RelevantAlterationOncogenicty", Oncogenicity.getByLevel(oncogenicityMapping.get(relevantAlt)).getDescription());
+                        service.insert(listFeedUrl1, row);
                         break;
                     }
                 }
@@ -89,10 +141,13 @@ public class validation {
                     if (mutationEffectMapping.containsKey(alt) && mutationEffectMapping.containsKey(relevantAlt) &&
                         mutationEffectMapping.get(alt) != null && mutationEffectMapping.get(relevantAlt) != null &&
                         !mutationEffectMapping.get(alt).equals(mutationEffectMapping.get(relevantAlt))) {
-                        result1 += alt.toString() + ": " + mutationEffectMapping.get(alt) +
-                            ". Relevant alteration: " + relevantAlt.toString() + ": " +
-                            mutationEffectMapping.get(relevantAlt) +  "\n";
-                        length1++;
+                        ListEntry row = new ListEntry();
+                        row.getCustomElements().setValueLocal("Gene", alt.getGene().getHugoSymbol());
+                        row.getCustomElements().setValueLocal("Alteration", alt.getAlteration());
+                        row.getCustomElements().setValueLocal("MutationEffect", mutationEffectMapping.get(alt) );
+                        row.getCustomElements().setValueLocal("RelevantAlteration", relevantAlt.getAlteration());
+                        row.getCustomElements().setValueLocal("RelevantAlterationMutationEffect", mutationEffectMapping.get(relevantAlt));
+                        service.insert(listFeedUrl2, row);
                         break;
                     }
                 }
@@ -108,65 +163,39 @@ public class validation {
                         relevantCount++;
                         if (relevantCount == relevantsSize - 1 && oncogenicityMapping.containsKey(alt) && oncogenicityMapping.get(relevantAlt).equals(Oncogenicity.UNKNOWN.getOncogenic()) 
                                 && mutationEffectMapping.containsKey(alt) && mutationEffectMapping.get(relevantAlt).equals(MutationEffect.UNKNOWN.getMutation_effect())) {
-                            result2 += relevantAlt.toString() + "\n";
-                            length2++;
+                            ListEntry row = new ListEntry();
+                        row.getCustomElements().setValueLocal("Gene", relevantAlt.getGene().getHugoSymbol());
+                        row.getCustomElements().setValueLocal("Alteration", relevantAlt.getAlteration());
+                        service.insert(listFeedUrl3, row);
+
                         }
                     }
                 }
                 if (!oncogenicityMapping.containsKey(alt) && !mutationEffectMapping.containsKey(alt) && !VUSAlterations.contains(alt) && !specialAlterations.contains(alt.getAlteration()) && !altsWithDescriptions.contains(alt)) {
-                    result3 += alt.toString() + "\n";
-                    length3++;
+                    ListEntry row = new ListEntry();
+                        row.getCustomElements().setValueLocal("Gene", alt.getGene().getHugoSymbol());
+                        row.getCustomElements().setValueLocal("Alteration", alt.getAlteration());
+                        service.insert(listFeedUrl4, row);
+
                 }
                 if (oncogenicityMapping.containsKey(alt) && mutationEffectMapping.containsKey(alt) && referencesMapping.get(alt).size() == 0) {
-                    result4 += alt.toString() + "\n";
-                    length4++;
+                    ListEntry row = new ListEntry();
+                        row.getCustomElements().setValueLocal("Gene", alt.getGene().getHugoSymbol());
+                        row.getCustomElements().setValueLocal("Alteration", alt.getAlteration());
+                        service.insert(listFeedUrl5, row);
+
                 }
                 if (multipleMutationEffects.containsKey(alt) && multipleMutationEffects.get(alt).size() > 1) {
-                    result5 += alt.toString() + "\n";
-                    length5++;
+                    ListEntry row = new ListEntry();
+                        row.getCustomElements().setValueLocal("Gene", alt.getGene().getHugoSymbol());
+                        row.getCustomElements().setValueLocal("Alteration", alt.getAlteration());
+                        service.insert(listFeedUrl6, row);
+
                 }
 
             }
             count++;
             System.out.println("Processing " + gene.getHugoSymbol() + "  " + 100 * count / genes.size() + "% finished");
         }
-        String output = "Rule 1 check result: There are " + Integer.toString(length0) + " variants whose oncogenicty is different with its relevant variants.\n";
-        output += result0;
-        
-        output += "******************************************************************************************\n";
-        output += "Rule 2 check result: There are " + Integer.toString(length1) + " variants whose mutation effect is different with its relevant variants.\n";
-        output += result1;
-
-        output += "******************************************************************************************\n";
-        output += "Rule 3 check result: There are " + Integer.toString(length2) + " variants that has unknown oncogenic and unknown mutation effect, and same for all its relevant variants.\n";
-        output += result2;
-
-        output += "******************************************************************************************\n";
-        output += "Rule 4 check result: There are " + Integer.toString(length3) + " variants that don't have oncogenic, mutation effect and descriptions, and not in the VUS list.\n";
-        output += result3;
-        
-        output += "******************************************************************************************\n";
-        output += "Rule 5 check result: There are " + Integer.toString(length4) + " variants that have oncogenic and mutation effect, but don't have any PMIDs.\n";
-        output += result4;
-
-        output += "******************************************************************************************\n";
-        output += "Rule 6 check result: There are " + Integer.toString(length5) + " variants that have multiple mutation effects (does not count relevant mutation effects)\n";
-        output += result5;
-
-        try {
-            File file = new File("validation-result.txt");
-            //if file doesnt exists, then create it
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            FileWriter fileWritter = new FileWriter(file.getName());
-            BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-            bufferWritter.write(output);
-            bufferWritter.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 }
