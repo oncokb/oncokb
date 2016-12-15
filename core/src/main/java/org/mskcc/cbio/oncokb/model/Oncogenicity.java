bd2cc742cededcac7a3fd8b98643d15f71971ba6
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2016-12-15 12:02:55 -0500

    compare oncogenicity

--- core/src/main/java/org/mskcc/cbio/oncokb/model/Oncogenicity.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/model/Oncogenicity.java
@@ -2,7 +2,10 @@
 
 package org.mskcc.cbio.oncokb.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -40,6 +43,17 @@ public enum Oncogenicity {
      */
     public static Oncogenicity getByLevel(String level) {
         return map.get(level);
+    }
+    
+    public static int compare(Oncogenicity o1, Oncogenicity o2){
+        //0 indicates o1 has the same oncogenicity with o2
+        //positive number indicates o1 has higher oncogenicity than o2
+        //negative number indicates o2 has higher oncogenicity than o1
+        List<String> oncogenicityValues = new ArrayList<>(Arrays.asList("Inconclusive", "Likely Neutral", "Likely Oncogenic", "Oncogenic"));
+        if(o1 == null && o2 == null)return 0;
+        else if(o1 == null) return -1;
+        else if(o2 == null)return 1;
+        else return oncogenicityValues.indexOf(o1.getOncogenic()) - oncogenicityValues.indexOf(o2.getOncogenic());
     }
 
 }
