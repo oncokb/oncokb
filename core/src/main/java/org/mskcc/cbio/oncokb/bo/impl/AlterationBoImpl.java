359390a3c731ce3fcd4501401484fd4a87279b4d
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-03-05 16:12:46 -0500

    Map Truncating Mutations
    For Fusion, if no Fusions has been curated, map Truncating Mutations;
    For Deletion, if no Deletion has been curated, map Truncating Mutations;
    For Translocation, map Truncating Mutations;
    For Inversion, map Truncating Mutations;
    This solves #401

--- core/src/main/java/org/mskcc/cbio/oncokb/bo/impl/AlterationBoImpl.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/bo/impl/AlterationBoImpl.java
@@ -101,76 +101,99 @@ public class AlterationBoImpl extends GenericBoImpl<Alteration, AlterationDao> i
         alterations.addAll(findMutationsByConsequenceAndPosition(alteration.getGene(), anyConsequence, alteration.getProteinStart(), alteration.getProteinEnd(), fullAlterations));
 
         //If alteration contains 'fusion'
-        if (alteration.getAlteration() != null && alteration.getAlteration().toLowerCase().contains("fusion")) {
-            boolean matchFusionForBothPatnerGenes = false;
-            if (matchFusionForBothPatnerGenes) {
-                GeneBo geneBo = ApplicationContextSingleton.getGeneBo();
-                String fusion = alteration.getAlteration(); // e.g. TRB-NKX2-1 fusion
-                int ix = fusion.toLowerCase().indexOf("fusion");
-                if (ix >= 0) {
-                    Gene gene = alteration.getGene();
-                    if (gene.getEntrezGeneId() > 0) {
-                        // find fusions annotated in the other gene
-                        String symbol = gene.getHugoSymbol();
-                        String genes = fusion.substring(0, ix);
-                        int ixg = genes.indexOf(symbol);
-                        if (ixg < 0) {
-                            System.err.println(fusion + " was under " + symbol);
+        if (alteration.getAlteration() != null) {
+            Alteration truncatingMutation = findAlteration(alteration.getGene(), alteration.getAlterationType(), "Truncating Mutations");
+            if (alteration.getAlteration().toLowerCase().contains("fusion")) {
+                boolean matchFusionForBothPatnerGenes = false;
+                if (matchFusionForBothPatnerGenes) {
+                    GeneBo geneBo = ApplicationContextSingleton.getGeneBo();
+                    String fusion = alteration.getAlteration(); // e.g. TRB-NKX2-1 fusion
+                    int ix = fusion.toLowerCase().indexOf("fusion");
+                    if (ix >= 0) {
+                        Gene gene = alteration.getGene();
+                        if (gene.getEntrezGeneId() > 0) {
+                            // find fusions annotated in the other gene
+                            String symbol = gene.getHugoSymbol();
+                            String genes = fusion.substring(0, ix);
+                            int ixg = genes.indexOf(symbol);
+                            if (ixg < 0) {
+                                System.err.println(fusion + " was under " + symbol);
+                            } else {
+                                String theOtherGene = genes.replace(symbol, "")
+                                    .replaceAll("-", " ").trim() // trim -
+                                    .replaceAll(" ", "-");
+
+                                Gene tog = geneBo.findGeneByHugoSymbol(theOtherGene);
+                                if (tog != null) {
+                                    String reverse;
+                                    if (ixg == 0) {
+                                        reverse = tog.getHugoSymbol() + "-" + symbol + " fusion";
+                                    } else {
+                                        reverse = symbol + "-" + tog.getHugoSymbol() + " fusion";
+                                    }
+
+                                    Alteration toa = findAlteration(tog, alteration.getAlterationType(), reverse);
+                                    if (toa != null) {
+                                        alterations.add(toa);
+                                    }
+
+                                    toa = findAlteration(tog, alteration.getAlterationType(), "fusions");
+                                    if (toa != null) {
+                                        alterations.add(toa);
+                                    }
+                                }
+                            }
                         } else {
-                            String theOtherGene = genes.replace(symbol, "")
-                                .replaceAll("-", " ").trim() // trim -
-                                .replaceAll(" ", "-");
-
-                            Gene tog = geneBo.findGeneByHugoSymbol(theOtherGene);
-                            if (tog != null) {
-                                String reverse;
-                                if (ixg == 0) {
-                                    reverse = tog.getHugoSymbol() + "-" + symbol + " fusion";
-                                } else {
-                                    reverse = symbol + "-" + tog.getHugoSymbol() + " fusion";
-                                }
-
-                                Alteration toa = findAlteration(tog, alteration.getAlterationType(), reverse);
+                            if (gene.getGeneAliases().size() == 2) {
+                                String[] aliases = gene.getGeneAliases().toArray(new String[0]);
+                                Gene gene0 = geneBo.findGeneByHugoSymbol(aliases[0]);
+                                Gene gene1 = geneBo.findGeneByHugoSymbol(aliases[1]);
+                                Alteration toa = findAlteration(gene1, alteration.getAlterationType(), aliases[0] + "-" + aliases[1] + " fusion");
                                 if (toa != null) {
                                     alterations.add(toa);
                                 }
-
-                                toa = findAlteration(tog, alteration.getAlterationType(), "fusions");
+                                toa = findAlteration(gene0, alteration.getAlterationType(), aliases[1] + "-" + aliases[0] + " fusion");
                                 if (toa != null) {
                                     alterations.add(toa);
                                 }
-                            }
-                        }
-                    } else {
-                        if (gene.getGeneAliases().size() == 2) {
-                            String[] aliases = gene.getGeneAliases().toArray(new String[0]);
-                            Gene gene0 = geneBo.findGeneByHugoSymbol(aliases[0]);
-                            Gene gene1 = geneBo.findGeneByHugoSymbol(aliases[1]);
-                            Alteration toa = findAlteration(gene1, alteration.getAlterationType(), aliases[0] + "-" + aliases[1] + " fusion");
-                            if (toa != null) {
-                                alterations.add(toa);
-                            }
-                            toa = findAlteration(gene0, alteration.getAlterationType(), aliases[1] + "-" + aliases[0] + " fusion");
-                            if (toa != null) {
-                                alterations.add(toa);
                             }
                         }
                     }
                 }
+
+                //the alteration 'fusions' should be injected into alteration list
+                Alteration alt = findAlteration(alteration.getGene(), alteration.getAlterationType(), "fusions");
+                if (alt != null) {
+                    alterations.add(alt);
+                } else {
+                    // If no fusions curated, check the Truncating Mutations.
+                    if (truncatingMutation != null) {
+                        alterations.add(alt);
+                    }
+                }
             }
 
-            //the alteration 'fusions' should be injected into alteration list
-            Alteration alt = findAlteration(alteration.getGene(), alteration.getAlterationType(), "fusions");
-            if (alt != null) {
-                alterations.add(alt);
-            } else {
+            // Map Truncating Mutations to Translocation and Inversion
+            // These two are all structural variants, need a better way to model them.
+            if (alteration.getAlteration().toLowerCase().equals("translocation")
+                || alteration.getAlteration().toLowerCase().equals("inversion")) {
                 // If no fusions curated, check the Truncating Mutations.
-                alt = findAlteration(alteration.getGene(), alteration.getAlterationType(), "Truncating Mutations");
+                if (truncatingMutation != null) {
+                    alterations.add(truncatingMutation);
+                }
+            }
+
+            // If no Deletion curated, attach Truncating Mutations
+            if (alteration.getAlteration().toLowerCase().equals("deletion")) {
+                Alteration alt = findAlteration(alteration.getGene(), alteration.getAlterationType(), "Deletion");
                 if (alt != null) {
+                    alterations.add(alt);
+                } else if (truncatingMutation != null) {
                     alterations.add(alt);
                 }
             }
         }
+
 
         // Looking for inferred mutations.
         EvidenceBo evidenceBo = ApplicationContextSingleton.getEvidenceBo();
