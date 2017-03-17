cf6a2bdcd30d9982bbc204b8838e1636aa01e8a6
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-03-17 17:29:24 -0400

    Fix curation website issue due to backend envelope change

--- web/yo/app/scripts/controllers/gene.js
+++ web/yo/app/scripts/controllers/gene.js
@@ -2060,7 +2060,7 @@ angular.module('oncokbApp')
                 var defaultPlaceHolder = 'No suggestion found. Please curate according to literature.';
                 DatabaseConnector.getSuggestedVariants()
                     .then(function(resp) {
-                        if (resp && _.isArray(resp.data) && resp.data.length > 0) {
+                        if (_.isArray(resp) && resp.length > 0) {
                             $scope.suggestedMutations = resp.data;
                         } else {
                             $scope.suggestedMutations = [];
