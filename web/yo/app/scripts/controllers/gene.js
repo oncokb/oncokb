6065af9bd9fc7e18b054d53153c6ac619526d5cc
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-03-20 13:50:33 -0400

    Minor bug

--- web/yo/app/scripts/controllers/gene.js
+++ web/yo/app/scripts/controllers/gene.js
@@ -2061,7 +2061,7 @@ angular.module('oncokbApp')
                 DatabaseConnector.getSuggestedVariants()
                     .then(function(resp) {
                         if (_.isArray(resp) && resp.length > 0) {
-                            $scope.suggestedMutations = resp.data;
+                            $scope.suggestedMutations = resp;
                         } else {
                             $scope.suggestedMutations = [];
                         }
