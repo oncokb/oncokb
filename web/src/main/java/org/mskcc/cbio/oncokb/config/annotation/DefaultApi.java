79be6106c9d37a6eb76d0ba4eb58014c0927616d
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-04-14 12:42:26 -0400

    Add API annotation

---
+++ web/src/main/java/org/mskcc/cbio/oncokb/config/annotation/DefaultApi.java
@@ -0,0 +1,13 @@
+package org.mskcc.cbio.oncokb.config.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Hongxin on 4/13/17.
+ */
+
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DefaultApi {
+}
