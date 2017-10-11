0eed756d54ace8b7d5b1beadfcd2596ea65c4097
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-10-11 12:56:46 -0400

    Include all resolveHGVS logics from genome nexus
    Sort set

--- core/src/main/java/org/mskcc/cbio/oncokb/genomenexus/VEPDetailedEnrichmentService.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/genomenexus/VEPDetailedEnrichmentService.java
@@ -35,7 +35,12 @@ package org.mskcc.cbio.oncokb.genomenexus;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Benjamin Gross
@@ -50,20 +55,48 @@ public class VEPDetailedEnrichmentService {
         {"Leu", "L"}, {"Lys", "K"}, {"Met", "M"}, {"Phe", "F"}, {"Pro", "P"}, {"Ser", "S"},
         {"Thr", "T"}, {"Trp", "W"}, {"Tyr", "Y"}, {"Val", "V"}, {"Xxx", "X"}, {"Ter", "*"}
     };
+    private Set<String> spliceSiteVariants = new HashSet<String>(Arrays.asList(
+        "splice_acceptor_variant", "splice_donor_variant", "splice_region_variant"));
+    private Pattern cDnaExtractor = Pattern.compile(".*[cn].-?\\*?(\\d+).*");
 
     public VariantAnnotation enrich(VariantAnnotation variantAnnotation) {
         if (variantAnnotation.getTranscriptConsequences() != null) {
             for (TranscriptConsequence transcriptConsequence : variantAnnotation.getTranscriptConsequences()) {
                 transcriptConsequence.setCodonChange(resolveCodonChange(transcriptConsequence.getCodons()));
                 transcriptConsequence.setConsequence(resolveConsequence(transcriptConsequence.getConsequenceTerms()));
-                transcriptConsequence.setHgvspShort(resolveHgvspShort(transcriptConsequence.getHgvsp()));
+                transcriptConsequence.setHgvspShort(resolveHgvspShort(transcriptConsequence));
                 transcriptConsequence.setRefSeq(resolveRefSeq(transcriptConsequence.getRefseqTranscriptIds()));
             }
         }
         return variantAnnotation;
     }
 
-    public String resolveHgvspShort(String hgvsp) {
+    public String resolveHgvspShort(TranscriptConsequence transcriptConsequence) {
+        String hgvsp = "";
+        if (transcriptConsequence != null) {
+            if (transcriptConsequence.getHgvsp() != null) {
+                hgvsp = getProcessedHgvsp(transcriptConsequence.getHgvsp());
+                for (int i = 0; i < 24; i++) {
+                    if (hgvsp.contains(aa3to1[i][0])) {
+                        hgvsp = hgvsp.replaceAll(aa3to1[i][0], aa3to1[i][1]);
+                    }
+                }
+            } else if (transcriptConsequence.getHgvsc() != null && spliceSiteVariants.contains(transcriptConsequence.getConsequenceTerms().get(0))) {
+                Integer cPos = 0;
+                Integer pPos = 0;
+                Matcher m = cDnaExtractor.matcher(transcriptConsequence.getHgvsc());
+                if (m.matches()) {
+                    cPos = Integer.parseInt(m.group(1));
+                    cPos = cPos < 1 ? 1 : cPos;
+                    pPos = (int)Math.ceil(((cPos + cPos % 3) / 3.0));
+                    hgvsp = "p.X" + String.valueOf(pPos) + "_splice";
+                }
+            } else {
+                // try to salvage using protein_start, amino_acids, and consequence_terms
+                hgvsp = resolveHgvspShortFromAAs(transcriptConsequence);
+            }
+        }
+
         if (hgvsp != null) {
             hgvsp = getProcessedHgvsp(hgvsp);
             for (int i = 0; i < 24; i++) {
@@ -72,6 +105,29 @@ public class VEPDetailedEnrichmentService {
                 }
             }
         }
+        return hgvsp;
+    }
+
+    private String resolveHgvspShortFromAAs(TranscriptConsequence transcriptConsequence) {
+        String hgvsp = "";
+        try {
+            String[] aaParts = transcriptConsequence.getAminoAcids().split("/");
+            if (transcriptConsequence.getConsequenceTerms() != null && transcriptConsequence.getConsequenceTerms().get(0).equals("inframe_insertion")) {
+                hgvsp = aaParts[1].substring(0, 1) + transcriptConsequence.getProteinStart() + "_" + aaParts[1].substring(1, 2) + "ins" +
+                    transcriptConsequence.getProteinEnd() + aaParts[1].substring(2);
+            } else if (transcriptConsequence.getConsequenceTerms() != null && transcriptConsequence.getConsequenceTerms().get(0).equals("inframe_deletion")) {
+                hgvsp = aaParts[0] + "del";
+            } else {
+                hgvsp = aaParts[0] + transcriptConsequence.getProteinStart();
+                if (transcriptConsequence.getConsequenceTerms() != null && transcriptConsequence.getConsequenceTerms().get(0).equals("frameshift_variant")) {
+                    hgvsp += "fs";
+                } else {
+                    hgvsp += aaParts[1];
+                }
+            }
+        } catch (NullPointerException e) {
+        }
+
         return hgvsp;
     }
 
