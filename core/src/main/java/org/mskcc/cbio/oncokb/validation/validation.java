7315b35682bc3d5ba9aff911edb60741ee42ee83
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-10-19 01:50:42 -0400

    Disable rule 3 and 5

--- core/src/main/java/org/mskcc/cbio/oncokb/validation/validation.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/validation/validation.java
@@ -200,26 +200,26 @@ public class validation {
 //                        break;
 //                    }
 //                }
-                if (oncogenicityMapping.containsKey(alt)
-                    && oncogenicityMapping.get(alt) != null
-                    && oncogenicityMapping.get(alt).equals(Oncogenicity.INCONCLUSIVE.getOncogenic())
-                    && mutationEffectMapping.containsKey(alt)
-                    && mutationEffectMapping.get(alt) != null
-                    && mutationEffectMapping.get(alt).equals(MutationEffect.INCONCLUSIVE.getMutationEffect())) {
-                    Integer relevantsSize = relevantAlts.size();
-                    Integer relevantCount = 0;
-                    for (Alteration relevantAlt : relevantAlts) {
-                        relevantCount++;
-                        if (relevantCount == relevantsSize - 1 && oncogenicityMapping.containsKey(alt) && oncogenicityMapping.get(relevantAlt) != null && oncogenicityMapping.get(relevantAlt).equals(Oncogenicity.INCONCLUSIVE.getOncogenic())
-                            && mutationEffectMapping.containsKey(alt) && mutationEffectMapping.get(relevantAlt).equals(MutationEffect.INCONCLUSIVE.getMutationEffect())) {
-                            ListEntry row = new ListEntry();
-                            setValue(row, "Gene", relevantAlt.getGene().getHugoSymbol());
-                            setValue(row, "Alteration", relevantAlt.getAlteration());
-                            service.insert(listFeedUrl3, row);
-
-                        }
-                    }
-                }
+//                if (oncogenicityMapping.containsKey(alt)
+//                    && oncogenicityMapping.get(alt) != null
+//                    && oncogenicityMapping.get(alt).equals(Oncogenicity.INCONCLUSIVE.getOncogenic())
+//                    && mutationEffectMapping.containsKey(alt)
+//                    && mutationEffectMapping.get(alt) != null
+//                    && mutationEffectMapping.get(alt).equals(MutationEffect.INCONCLUSIVE.getMutationEffect())) {
+//                    Integer relevantsSize = relevantAlts.size();
+//                    Integer relevantCount = 0;
+//                    for (Alteration relevantAlt : relevantAlts) {
+//                        relevantCount++;
+//                        if (relevantCount == relevantsSize - 1 && oncogenicityMapping.containsKey(alt) && oncogenicityMapping.get(relevantAlt) != null && oncogenicityMapping.get(relevantAlt).equals(Oncogenicity.INCONCLUSIVE.getOncogenic())
+//                            && mutationEffectMapping.containsKey(alt) && mutationEffectMapping.get(relevantAlt).equals(MutationEffect.INCONCLUSIVE.getMutationEffect())) {
+//                            ListEntry row = new ListEntry();
+//                            setValue(row, "Gene", relevantAlt.getGene().getHugoSymbol());
+//                            setValue(row, "Alteration", relevantAlt.getAlteration());
+//                            service.insert(listFeedUrl3, row);
+//
+//                        }
+//                    }
+//                }
 //                if (!oncogenicityMapping.containsKey(alt) && !mutationEffectMapping.containsKey(alt) && !VUSAlterations.contains(alt) && !specialAlterations.contains(alt.getAlteration()) && !altsWithDescriptions.contains(alt)) {
 //                    ListEntry row = new ListEntry();
 //                    setValue(row, "Gene", alt.getGene().getHugoSymbol());
@@ -227,13 +227,13 @@ public class validation {
 //                    service.insert(listFeedUrl4, row);
 //
 //                }
-                if (oncogenicityMapping.containsKey(alt) && mutationEffectMapping.containsKey(alt) && referencesMapping.get(alt).size() == 0) {
-                    ListEntry row = new ListEntry();
-                    setValue(row, "Gene", alt.getGene().getHugoSymbol());
-                    setValue(row, "Alteration", alt.getAlteration());
-                    service.insert(listFeedUrl5, row);
-
-                }
+//                if (oncogenicityMapping.containsKey(alt) && mutationEffectMapping.containsKey(alt) && referencesMapping.get(alt).size() == 0) {
+//                    ListEntry row = new ListEntry();
+//                    setValue(row, "Gene", alt.getGene().getHugoSymbol());
+//                    setValue(row, "Alteration", alt.getAlteration());
+//                    service.insert(listFeedUrl5, row);
+//
+//                }
 //                if (multipleMutationEffects.containsKey(alt) && multipleMutationEffects.get(alt).size() > 1) {
 //                    ListEntry row = new ListEntry();
 //                    setValue(row, "Gene", alt.getGene().getHugoSymbol());
