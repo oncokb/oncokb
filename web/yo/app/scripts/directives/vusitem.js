eb78c1679065d4cc483cb9aab527a431ae2a67e5
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2018-07-05 17:04:37 -0400

    vus data reference bug fix

--- web/yo/app/scripts/directives/vusitem.js
+++ web/yo/app/scripts/directives/vusitem.js
@@ -12,13 +12,12 @@ angular.module('oncokbApp')
             templateUrl: 'views/vusItem.html',
             restrict: 'E',
             scope: {
-                vus: '=',
                 index: '=',
                 gene: '=',
                 vusUpdateInGene: '&vusUpdate'
             },
             link: function postLink(scope) {
-                scope.variant = scope.vus.vus[scope.index];
+                scope.variant = $rootScope.vusFire.vus[scope.index];
                 scope.dt = new Date(Number(scope.variant.time[scope.variant.time.length - 1].value));
                 scope.dtBy = scope.variant.time[scope.variant.time.length - 1].by.name;
                 scope.dateOptions = {
@@ -30,7 +29,7 @@ angular.module('oncokbApp')
                     if (n !== o) {
                         var user = $rootScope.me;
                         var timeStamp = new FirebaseModel.TimeStamp(user.name, user.email);
-                        scope.vus.vus[scope.index].time.push(timeStamp);
+                        $rootScope.vusFire.vus[scope.index].time.push(timeStamp);
                         scope.dtBy = user.name;
                         scope.vusUpdate();
                     }
@@ -40,7 +39,7 @@ angular.module('oncokbApp')
                 $scope.remove = function() {
                     var dlg = dialogs.confirm('Confirmation', 'Are you sure you want to delete this entry?');
                     dlg.result.then(function() {
-                        $scope.vus.vus.splice($scope.index, 1);
+                        $rootScope.vusFire.vus.splice($scope.index, 1);
                         $scope.vusUpdate();
                     }, function() {
                     });
