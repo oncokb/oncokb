54216f89a11b3dd87cfaa47d180ba74572efd627
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-07-13 16:26:35 -0400

    Do not resave exist article again

--- web/src/main/java/org/mskcc/cbio/oncokb/controller/DriveAnnotationParser.java
+++ web/src/main/java/org/mskcc/cbio/oncokb/controller/DriveAnnotationParser.java
@@ -788,9 +788,11 @@ public class DriveAnnotationParser {
                 Article doc = articleBo.findArticleByPmid(pmid);
                 if (doc == null) {
                     doc = NcbiEUtils.readPubmedArticle(pmid);
+                    if (doc != null) {
+                        articleBo.save(doc);
+                    }
                 }
                 if (doc != null) {
-                    articleBo.save(doc);
                     docs.add(doc);
                 }
             }
