2eeb873a25ce2830f3c47b145d65ce9a6f6dca21
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-08-06 11:54:56 -0400

    Make hotspot mutations cache gene based

--- core/src/main/java/org/mskcc/cbio/oncokb/util/HotspotUtils.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/util/HotspotUtils.java
@@ -11,35 +11,46 @@ import org.mskcc.cbio.oncokb.model.VariantConsequence;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Hongxin on 11/03/16.
  */
 public class HotspotUtils {
-    private static List<SingleResidueHotspotMutation> hotspotMutations = null;
+    private static Map<Gene, List<SingleResidueHotspotMutation>> hotspotMutations = new HashMap<>();
 
     public static void getHotspotsFromRemote() {
+        List<SingleResidueHotspotMutation> hotspots = new ArrayList<>();
         try {
             String cancerHotspotsUrl = PropertiesUtils.getProperties("cancerhotspots.single");
             String response = HttpUtils.postRequest(cancerHotspotsUrl, "");
-            hotspotMutations = new ObjectMapper().readValue(response, new TypeReference<List<SingleResidueHotspotMutation>>() {
+            hotspots = new ObjectMapper().readValue(response, new TypeReference<List<SingleResidueHotspotMutation>>() {
             });
         } catch (Exception e) {
             System.out.println("Fail to reach CancerHotspot endpoint. Fetch local file.");
             Gson gson = new GsonBuilder().create();
             SingleResidueHotspotMutation[] mutations = gson.fromJson(new BufferedReader(new InputStreamReader(HotspotUtils.class.getResourceAsStream("/data/cancer-hotspots-public-v2.json"))), SingleResidueHotspotMutation[].class);
-            hotspotMutations = new ArrayList<>(Arrays.asList(mutations));
-            if (hotspotMutations == null) {
-                hotspotMutations = new ArrayList<>();
+            hotspots = new ArrayList<>(Arrays.asList(mutations));
+        }
+        parseData(hotspots);
+    }
+
+    private static void parseData(List<SingleResidueHotspotMutation> hotspots) {
+        if (hotspots != null) {
+            for (SingleResidueHotspotMutation hotspotMutation : hotspots) {
+                Gene gene = GeneUtils.getGeneByHugoSymbol(hotspotMutation.getHugoSymbol());
+                if (gene != null) {
+                    if (!hotspotMutations.containsKey(gene)) {
+                        hotspotMutations.put(gene, new ArrayList<SingleResidueHotspotMutation>());
+                    }
+                    hotspotMutations.get(gene).add(hotspotMutation);
+                }
             }
         }
     }
 
-    public static List<SingleResidueHotspotMutation> getHotspots() {
-        if (hotspotMutations == null) {
+    public static Map<Gene, List<SingleResidueHotspotMutation>> getHotspots() {
+        if (hotspotMutations.keySet().size() == 0) {
             getHotspotsFromRemote();
         }
         return hotspotMutations;
@@ -64,29 +75,28 @@ public class HotspotUtils {
                 if (proteinEnd == null) {
                     proteinEnd = proteinStart;
                 }
-                for (SingleResidueHotspotMutation hotspotMutation : hotspotMutations) {
-                    if (hotspotMutation.getType().equals("single residue")
-                        && hotspotMutation.getAminoAcidPosition() != null
-                        && proteinStart >= hotspotMutation.getAminoAcidPosition().getStart()
-                        && proteinEnd <= hotspotMutation.getAminoAcidPosition().getEnd()) {
 
-                        // The gene in hotspot may refer to gene alias in OncoKB
-                        Gene gene = GeneUtils.getGeneByHugoSymbol(hotspotMutation.getHugoSymbol());
-                        if (gene != null && gene.equals(alteration.getGene())) {
+                if (hotspotMutations.containsKey(alteration.getGene())) {
+                    for (SingleResidueHotspotMutation hotspotMutation : hotspotMutations.get(alteration.getGene())) {
+                        if (hotspotMutation.getType().equals("single residue")
+                            && hotspotMutation.getAminoAcidPosition() != null
+                            && proteinStart >= hotspotMutation.getAminoAcidPosition().getStart()
+                            && proteinEnd <= hotspotMutation.getAminoAcidPosition().getEnd()) {
                             isHotspot = true;
                             break;
                         }
                     }
                 }
             } else if (alteration.getConsequence().equals(insertion) || alteration.getConsequence().equals(deletion)) {
-                for (SingleResidueHotspotMutation hotspotMutation : hotspotMutations) {
-                    if (hotspotMutation.getType().equals("in-frame indel")
-                        && hotspotMutation.getHugoSymbol().equals(alteration.getGene().getHugoSymbol())
-                        && hotspotMutation.getAminoAcidPosition() != null
-                        && proteinEnd >= hotspotMutation.getAminoAcidPosition().getStart()
-                        && proteinStart <= hotspotMutation.getAminoAcidPosition().getEnd()) {
-                        isHotspot = true;
-                        break;
+                if (hotspotMutations.containsKey(alteration.getGene())) {
+                    for (SingleResidueHotspotMutation hotspotMutation : hotspotMutations.get(alteration.getGene())) {
+                        if (hotspotMutation.getType().equals("in-frame indel")
+                            && hotspotMutation.getAminoAcidPosition() != null
+                            && proteinEnd >= hotspotMutation.getAminoAcidPosition().getStart()
+                            && proteinStart <= hotspotMutation.getAminoAcidPosition().getEnd()) {
+                            isHotspot = true;
+                            break;
+                        }
                     }
                 }
             }
