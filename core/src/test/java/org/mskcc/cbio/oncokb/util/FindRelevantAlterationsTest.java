137faf83bdd0b0c6cddecd17e3ee49f7dc10af13
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-08-17 14:54:43 -0400

    Fix the tests based on the latest data

--- core/src/test/java/org/mskcc/cbio/oncokb/util/FindRelevantAlterationsTest.java
+++ core/src/test/java/org/mskcc/cbio/oncokb/util/FindRelevantAlterationsTest.java
@@ -33,7 +33,7 @@ public class FindRelevantAlterationsTest {
         return Arrays.asList(
             new String[][]{
                 // Critical cases
-                {"BRAF", "V600E", null, "V600E, V600A, V600D, V600G, V600K, V600L, V600M, V600Q, V600R, VK600EI, Oncogenic Mutations"},
+                {"BRAF", "V600E", null, "V600E, V600A, V600D, V600G, V600K, V600L, V600M, V600Q, V600R, VK600EI, V600, Oncogenic Mutations"},
                 {"SMARCB1", "R374Q", null, "R374Q, R374W, Oncogenic Mutations"},
 
                 // Check Fusions
@@ -41,20 +41,20 @@ public class FindRelevantAlterationsTest {
                 {"CTCF", "CTCF-intragenic", null, "Truncating Mutations"},
                 {"CTCF", "CTCF intragenic", null, "Truncating Mutations"},
                 {"CTCF", "Intragenic", null, "Truncating Mutations"},
-                {"NOTCH1", "NOTCH1-intragenic", null, "Fusions, Gain-of-function Mutations"},
+                {"NOTCH1", "NOTCH1-intragenic", null, "Fusions"},
 
                 // Tumor suppressor should be mapped with Truncating Mutations. (The code does not check whether gene
                 // is tumor suppressor, just check whether Fusions is curated, is not, link Truncating Mutations)
-                {"PIK3R1", "KCTD16-PIK3R1 fusion", null, "KCTD16-PIK3R1 fusion, Truncating Mutations, Oncogenic Mutations"},
+                {"PIK3R1", "KCTD16-PIK3R1 fusion", null, "KCTD16-PIK3R1 fusion, Truncating Mutations"},
 
                 // General truncating consequence should be associated with Truncating Mutations
                 // Check splice
                 {"TP53", "X33_splice", null, "X33_splice, Truncating Mutations, Oncogenic Mutations"},
-                {"MET", "X1010_splice", null, "X1010_splice, 963_D1010splice, 981_1028splice, Oncogenic Mutations"},
-                {"MET", "X1010splice", null, "X1010_splice, 963_D1010splice, 981_1028splice, Oncogenic Mutations"},
+                {"MET", "X1010_splice", null, "X1010_splice, 963_D1010splice, 981_1028splice, 963_1028splice, Oncogenic Mutations"},
+                {"MET", "X1010splice", null, "X1010_splice, 963_D1010splice, 981_1028splice, 963_1028splice, Oncogenic Mutations"},
 
                 // Check stop_gained
-                {"MAP2K4", "R304*", null, "R304*, Truncating Mutations, Oncogenic Mutations"},
+                {"MAP2K4", "R304*", null, "R304*, Truncating Mutations"},
 
                 // EGFR exon deletion
                 {"EGFR", "vIII", null, "vIII, Oncogenic Mutations"},
@@ -67,11 +67,12 @@ public class FindRelevantAlterationsTest {
                 // Check range
                 {"MED12", "G44S", null, "G44S, G44A, G44C, G44D, G44V, 34_68mut"},
                 {"MED12", "G44D", null, "G44D, G44A, G44C, G44S, G44V, 34_68mut"},
-                {"NOTCH1", "Q2405Rfs*17", null, "Q2405Rfs*17, T2375_K2555trunc, Gain-of-function Mutations"},
+                {"NOTCH1", "Q2405Rfs*17", null, "Q2405Rfs*17, T2375_K2555trunc"},
 
                 // VUS should get mapped to hotspot VUS, but should not get Oncogenic Mutations from the hotspot VUS.
                 // In this case VUS N109_R113del is covered by VUS I99_R113del, and I99_R113del is a hotpot.
-                {"MAP2K1", "N109_R113del", null, "N109_R113del, I99_R113del"},
+                // No longer applicable
+//                {"MAP2K1", "N109_R113del", null, "N109_R113del, I99_R113del"},
 
                 // Range missense variant
                 {"PDGFRA", "D842I", null, "D842I, D842H, D842V, D842Y, D842_I843delinsIM, Oncogenic Mutations"},
@@ -87,13 +88,13 @@ public class FindRelevantAlterationsTest {
                 // 34 is in Exon 2, the Exon 2 should not be mapped.
                 {"MED12", "A34*", null, "Truncating Mutations"},
 
-                {"NOTCH1", "Q2405Rfs*17", null, "Q2405Rfs*17, T2375_K2555trunc, Gain-of-function Mutations"},
+                {"NOTCH1", "Q2405Rfs*17", null, "Q2405Rfs*17, T2375_K2555trunc"},
 
                 // Deletion
                 // With specific Deletion curated
                 {"BRCA2", "Deletion", null, "Deletion, Oncogenic Mutations"},
                 // Without specific Deletion curated
-                {"MAP2K4", "Deletion", null, "Truncating Mutations, Oncogenic Mutations"},
+                {"MAP2K4", "Deletion", null, "Truncating Mutations"},
 
                 // Abbreviated alterations
                 {"EGFR", "KDD", null, "Kinase Domain Duplication, Oncogenic Mutations"},
