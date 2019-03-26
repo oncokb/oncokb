72595614a016ce55baf829317ee74a832f91bc7f
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-03-26 16:59:17 -0400

    Document annotation endpoints with example

--- core/src/main/java/org/mskcc/cbio/oncokb/apiModels/annotation/AnnotateCopyNumberAlterationQuery.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/apiModels/annotation/AnnotateCopyNumberAlterationQuery.java
@@ -7,7 +7,6 @@ import org.mskcc.cbio.oncokb.model.CopyNumberAlterationType;
  * Created by Hongxin Zhang on 2019-03-25.
  */
 
-@ApiModel()
 public class AnnotateCopyNumberAlterationQuery extends AnnotationQuery implements java.io.Serializable {
     private QueryGene gene;
     private CopyNumberAlterationType copyNameAlterationType;
