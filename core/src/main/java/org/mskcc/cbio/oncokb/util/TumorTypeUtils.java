cb65eb852d111abfc5dba6853c6fa031c92740da
Hongxin Zhang <hongxin@cbio.mskcc.org>
2020-03-04 17:30:44 -0500

    Open websocket session and report empty clinical variant info

--- core/src/main/java/org/mskcc/cbio/oncokb/util/TumorTypeUtils.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/util/TumorTypeUtils.java
@@ -235,6 +235,20 @@ public class TumorTypeUtils {
         return null;
     }
 
+    public static String getTumorTypeName(TumorType tumorType) {
+        if (tumorType == null) {
+            return "";
+        }
+        if (tumorType.getName() != null) {
+            return tumorType.getName();
+        }
+        if (tumorType.getMainType() != null && tumorType.getMainType().getName() != null) {
+            return tumorType.getMainType().getName();
+        } else {
+            return "";
+        }
+    }
+
     /**
      * The 'All Tumors' OncoTree instance
      *
