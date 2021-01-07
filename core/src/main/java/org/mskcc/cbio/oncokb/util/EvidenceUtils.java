3770ead9ecba22325e16be54f838b99e227f2a4a
Hongxin Zhang <hongxin@cbio.mskcc.org>
2021-01-07 10:09:53 -0500

    Update EvidenceUtils.java

--- core/src/main/java/org/mskcc/cbio/oncokb/util/EvidenceUtils.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/util/EvidenceUtils.java
@@ -244,7 +244,7 @@ public class EvidenceUtils {
 
             // Get diagnostic implication evidences
             if (evidenceTypes.contains(EvidenceType.DIAGNOSTIC_IMPLICATION)) {
-                evidences.addAll(getEvidence(uniqueAlterationsWithoutAlternativeAlleles, Collections.singleton(EvidenceType.DIAGNOSTIC_IMPLICATION), matchedTumorType, downwardTumorTypes, levelOfEvidences));
+                evidences.addAll(getEvidence(uniqueAlterationsWithoutAlternativeAlleles, Collections.singleton(EvidenceType.DIAGNOSTIC_IMPLICATION), matchedTumorType, StringUtils.isEmpty(query.getQuery().getTumorType()) ? null : downwardTumorTypes, levelOfEvidences));
             }
 
             // Get other tumor type related evidences
@@ -253,8 +253,7 @@ public class EvidenceUtils {
             restTTevidenceTypes.remove(EvidenceType.DIAGNOSTIC_IMPLICATION);
             common = Sets.intersection(restTTevidenceTypes, evidenceTypes);
             if (common.size() > 0) {
-
-                evidences.addAll(getEvidence(uniqueAlterationsWithoutAlternativeAlleles, common, matchedTumorType, upwardTumorTypes, levelOfEvidences));
+                evidences.addAll(getEvidence(uniqueAlterationsWithoutAlternativeAlleles, common, matchedTumorType, StringUtils.isEmpty(query.getQuery().getTumorType()) ? null : upwardTumorTypes, levelOfEvidences));
             }
         }
 
