922cc94360b4e7216ab22e52d4aaf4ca7d6e0c02
Hongxin <5400599+zhx828@users.noreply.github.com>
2022-02-28 17:16:13 -0500

    Remove special rule on EGFR A763_Y764insFQEA

--- core/src/test/java/org/mskcc/cbio/oncokb/util/AlterationUtilsTest.java
+++ core/src/test/java/org/mskcc/cbio/oncokb/util/AlterationUtilsTest.java
@@ -59,7 +59,7 @@ public class AlterationUtilsTest extends TestCase
         for (Alteration alt : positionedAlterations) {
             alterations.add(alt.getAlteration());
         }
-        assertEquals("V600,V600 {excluding V600E ; V600K}", MainUtils.listToString(alterations.stream().sorted().collect(Collectors.toList()), ","));
+        assertEquals("V600,V600 {excluding V600E ; V600K}", MainUtils.listToString(alterations, ",", true));
 
         // non missense should not be annotated
         alteration = new Alteration();
@@ -125,8 +125,8 @@ public class AlterationUtilsTest extends TestCase
         assertEquals("The relevant alterations do not match", "V600E, V600K, V600_V601delinsEB, V600", relevantAltsName);
 
         alterations = AlterationUtils.getRelevantAlterations(DEFAULT_REFERENCE_GENOME, v600k, fullAlteration);
-        relevantAltsName = AlterationUtils.toString(alterations);
-        assertEquals("The relevant alterations do not match", "V600K, V600E, V600_V601delinsEB, V600_V601delinsEB {excluding V600E}, V600, V600 {excluding V600E}", relevantAltsName);
+        relevantAltsName = AlterationUtils.toString(alterations, true);
+        assertEquals("The relevant alterations do not match", "V600, V600 {excluding V600E}, V600E, V600K, V600_V601delinsEB, V600_V601delinsEB {excluding V600E}", relevantAltsName);
 
         // Test alteration with exclusion
         Alteration fusionA = generateAlteration(gene, "AKAP9-BRAF Fusion");
