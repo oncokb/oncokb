bd8eba592986b9dc5c6f19940e1508bf96da6cd2
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2022-10-28 15:10:38 -0400

    Add redis connection size properties and upgrade Redisson to v3.17.7 (#3281)
    * Update Redisson configuration:
    - Set subscription connection pool size to zero
    - Add properties to change master/slave connection pool sizes
    - Add redisson client name
    - Upgrade to Redisson 3.17.7

--- core/src/main/java/org/mskcc/cbio/oncokb/cache/CacheConfiguration.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/cache/CacheConfiguration.java
@@ -1,12 +1,18 @@
 package org.mskcc.cbio.oncokb.cache;
 
+import org.apache.commons.lang3.StringUtils;
 import org.mskcc.cbio.oncokb.cache.keygenerator.ConcatGenerator;
 import org.mskcc.cbio.oncokb.util.PropertiesUtils;
 import org.mskcc.oncokb.meta.enumeration.RedisType;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.SnappyCodecV2;
+import org.redisson.config.BaseConfig;
+import org.redisson.config.BaseMasterSlaveServersConfig;
+import org.redisson.config.ClusterServersConfig;
 import org.redisson.config.Config;
+import org.redisson.config.SentinelServersConfig;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheResolver;
@@ -17,7 +23,9 @@ import org.springframework.context.annotation.*;
 @EnableCaching
 @Conditional(EnableCacheCondition.class)
 public class CacheConfiguration {
+    
     private final int DEFAULT_TTL = 60;
+
     @Bean
     public RedissonClient redissonClient()
         throws Exception {
@@ -26,28 +34,40 @@ public class CacheConfiguration {
         String redisPassword = PropertiesUtils.getProperties("redis.password");
         String redisAddress = PropertiesUtils.getProperties("redis.address");
         String redisMasterName = PropertiesUtils.getProperties("redis.masterName");
+        String redisSlaveConnectionMinimumIdleSize = PropertiesUtils.getProperties("redis.slaveConnectionMinimumIdleSize");
+        String redisSlaveConnectionPoolSize = PropertiesUtils.getProperties("redis.slaveConnectionPoolSize");
+        String redisMasterConnectionMinimumIdleSize = PropertiesUtils.getProperties("redis.masterConnectionMinimumIdleSize");
+        String redisMasterConnectionPoolSize = PropertiesUtils.getProperties("redis.masterConnectionPoolSize");
 
         if (redisType.equals(RedisType.SINGLE.getType())) {
-            config
+            SingleServerConfig singleServerConfig = config
                 .useSingleServer()
                 .setAddress(redisAddress)
                 .setConnectionMinimumIdleSize(1)
                 .setConnectionPoolSize(2)
+                .setSubscriptionConnectionMinimumIdleSize(0)
+                .setSubscriptionConnectionPoolSize(0)
                 .setDnsMonitoringInterval(-1)
                 .setPassword(redisPassword);
+            setRedisClientName(singleServerConfig);
         } else if (redisType.equals(RedisType.SENTINEL.getType())) {
-            config
+            SentinelServersConfig sentinelConfig = config
                 .useSentinelServers()
                 .setMasterName(redisMasterName)
                 .setCheckSentinelsList(false)
                 .setDnsMonitoringInterval(-1)
                 .addSentinelAddress(redisAddress)
                 .setPassword(redisPassword);
+            setRedisConnectionPoolSize(sentinelConfig, redisSlaveConnectionMinimumIdleSize, redisSlaveConnectionPoolSize, redisMasterConnectionMinimumIdleSize, redisMasterConnectionPoolSize);
+            setRedisClientName(sentinelConfig);
         } else if (redisType.equals(RedisType.CLUSTER.getType())) {
-            config
-                .useClusterServers()
-                .addNodeAddress(redisAddress)
-                .setPassword(redisPassword);
+            ClusterServersConfig clusterConfig = 
+                config
+                    .useClusterServers()
+                    .addNodeAddress(redisAddress)
+                    .setPassword(redisPassword);
+            setRedisConnectionPoolSize(clusterConfig, redisSlaveConnectionMinimumIdleSize, redisSlaveConnectionPoolSize, redisMasterConnectionMinimumIdleSize, redisMasterConnectionPoolSize);
+            setRedisClientName(clusterConfig);
         } else {
             throw new Exception(
                 "The redis type " +
@@ -55,10 +75,38 @@ public class CacheConfiguration {
                     " is not supported. Only single, sentinel, and cluster are supported."
             );
         }
+
         // Instead of using GZip to compress data manually, we can configure Redisson to use
         // snappy codec. Redisson will serialize and compress our cache values.
         config.setCodec(new SnappyCodecV2());
         return Redisson.create(config);
+    }
+
+    private void setRedisConnectionPoolSize(BaseMasterSlaveServersConfig baseMasterSlaveServersConfig, String redisSlaveConnectionMinimumIdleSize, String redisSlaveConnectionPoolSize, String redisMasterConnectionMinimumIdleSize, String redisMasterConnectionPoolSize) {
+        baseMasterSlaveServersConfig.setSubscriptionConnectionMinimumIdleSize(0);
+        baseMasterSlaveServersConfig.setSubscriptionConnectionPoolSize(0);
+
+        if (StringUtils.isNotEmpty(redisSlaveConnectionMinimumIdleSize)) {
+            baseMasterSlaveServersConfig.setSlaveConnectionMinimumIdleSize(Integer.parseInt(redisSlaveConnectionMinimumIdleSize));
+        }
+        if (StringUtils.isNotEmpty(redisSlaveConnectionPoolSize)) {
+            baseMasterSlaveServersConfig.setSlaveConnectionPoolSize(Integer.parseInt(redisSlaveConnectionPoolSize));
+        }
+        if (StringUtils.isNotEmpty(redisMasterConnectionMinimumIdleSize)) {
+            baseMasterSlaveServersConfig.setMasterConnectionMinimumIdleSize(Integer.parseInt(redisMasterConnectionMinimumIdleSize));
+        }
+        if (StringUtils.isNotEmpty(redisMasterConnectionPoolSize)) {
+            baseMasterSlaveServersConfig.setMasterConnectionPoolSize(Integer.parseInt(redisMasterConnectionPoolSize));
+        }
+    }
+
+    private void setRedisClientName(BaseConfig baseConfig) {
+        String appName = PropertiesUtils.getProperties("app.name");
+        if (StringUtils.isNotEmpty(appName)) {
+            baseConfig.setClientName(appName);
+        } else {
+            baseConfig.setClientName("oncokb-core");
+        }
     }
 
     @Bean
