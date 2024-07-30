6d8401c2c6fd6f418ec1d31b1faa62dbdf259d3e
stephen gao <stephen.j.gao@gmail.com>
2024-07-30 16:21:04 -0400

    Website now supports 3 letter genes, improved tests

--- core/src/main/java/org/mskcc/cbio/oncokb/util/AminoAcidConverterUtils.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/util/AminoAcidConverterUtils.java
@@ -2,8 +2,9 @@ package org.mskcc.cbio.oncokb.util;
 
 public class AminoAcidConverterUtils {
 
-    //code copied from https://github.com/genome-nexus/genome-nexus/blob/3b4d0eefa2ff4976b632180d770c4073cc957ecc/component/src/main/java/org/cbioportal/genome_nexus/component/annotation/ProteinChangeResolver.java#L95
+    // code copied from https://github.com/genome-nexus/genome-nexus/blob/3b4d0eefa2ff4976b632180d770c4073cc957ecc/component/src/main/java/org/cbioportal/genome_nexus/component/annotation/ProteinChangeResolver.java#L95
 
+    // maps each letter to its corresponding three-letter code
     private static final String AA3TO1[][] = {
         {"Ala", "A"}, {"Arg", "R"}, {"Asn", "N"}, {"Asp", "D"}, {"Asx", "B"}, {"Cys", "C"},
         {"Glu", "E"}, {"Gln", "Q"}, {"Glx", "Z"}, {"Gly", "G"}, {"His", "H"}, {"Ile", "I"},
@@ -11,9 +12,10 @@ public class AminoAcidConverterUtils {
         {"Thr", "T"}, {"Trp", "W"}, {"Tyr", "Y"}, {"Val", "V"}, {"Xxx", "X"}, {"Ter", "*"}
     };
 
-
+    // converts a three-letter amino acid code to a one-letter amino acid code
     public static String resolveHgvspShortFromHgvsp(String hgvsp)
     {
+        // check if there's a digit in order to not mistakenly convert a normal alteration name
         if (hgvsp == null || !hgvsp.matches(".*\\d.*")) {
             return hgvsp;
         }
