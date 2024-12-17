0ca948dd7cc9e7116fe44638e8dc663efb7eb8c8
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2024-12-17 15:48:22 -0500

    Add reasoning for empty catch block

--- web/src/main/java/org/mskcc/cbio/oncokb/controller/advice/ExceptionTranslator.java
+++ web/src/main/java/org/mskcc/cbio/oncokb/controller/advice/ExceptionTranslator.java
@@ -23,10 +23,7 @@ public class ExceptionTranslator {
     @ExceptionHandler(org.oncokb.oncokb_transcript.ApiException.class)
     public final ResponseEntity<ApiHttpError> handleTranscriptApiException(org.oncokb.oncokb_transcript.ApiException ex, WebRequest request) {
         String path = request.getDescription(false).replaceFirst("uri=", "");
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        try {
-            httpStatus = HttpStatus.valueOf(ex.getCode());
-        } catch (IllegalArgumentException e) {}
+        HttpStatus httpStatus = getHttpStatusFromException(ex.getCode());
         ApiHttpError error = new ApiHttpError(path, httpStatus, ex.getMessage());
         return new ResponseEntity<>(error, httpStatus);
     }
@@ -34,15 +31,23 @@ public class ExceptionTranslator {
     @ExceptionHandler(org.genome_nexus.ApiException.class)
     public final ResponseEntity<ApiHttpError> handleTranscriptApiException(org.genome_nexus.ApiException ex, WebRequest request) {
         String path = request.getDescription(false).replaceFirst("uri=", "");
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        HttpStatus httpStatus = getHttpStatusFromException(ex.getCode());
         String message = ex.getMessage();
         if (StringUtils.isEmpty(message)) {
             message = "Error annotating variant(s) using Genome Nexus";
         }
-        try {
-            httpStatus = HttpStatus.valueOf(ex.getCode());
-        } catch (IllegalArgumentException e) {}
         ApiHttpError error = new ApiHttpError(path, httpStatus, message);
         return new ResponseEntity<>(error, httpStatus);
+    }
+
+    private HttpStatus getHttpStatusFromException(int statusCode) {
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        try {
+            httpStatus = HttpStatus.valueOf(statusCode);
+        } catch (IllegalArgumentException e) {
+            // Some exception may not provide a status code. In these cases
+            // we will use the default Internal Server Error code
+        }
+        return httpStatus;
     }
 }
