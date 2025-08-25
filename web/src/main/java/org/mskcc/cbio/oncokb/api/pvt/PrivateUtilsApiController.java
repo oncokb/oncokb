15dd54b325cc7d83b3cf3eedf841a6df89b31ce2
bprize15 <bprize15@gmail.com>
2025-08-25 14:35:43 -0400

    address pr comments

--- web/src/main/java/org/mskcc/cbio/oncokb/api/pvt/PrivateUtilsApiController.java
+++ web/src/main/java/org/mskcc/cbio/oncokb/api/pvt/PrivateUtilsApiController.java
@@ -441,23 +441,28 @@ public class PrivateUtilsApiController implements PrivateUtilsApi {
 
             List<Evidence> updatedEvidences = new ArrayList<>();
             Map<String, LevelOfEvidence> treatmentToHighestLevel = new HashMap<>();
-            for (Evidence evidence : response.getEvidences().stream().filter(evidence -> !evidence.getCancerTypes().isEmpty() && evidence.getCancerTypes().contains(uniqueTumorType)).collect(Collectors.toList())) {     
-                String treament = TreatmentUtils.getTreatmentName(evidence.getTreatments());       
-                if (treatmentToHighestLevel.get(treament) == null 
-                    || LevelUtils.compareLevel(evidence.getLevelOfEvidence(), treatmentToHighestLevel.get(treament), LevelUtils.THERAPEUTIC_SENSITIVE_LEVELS) < 0
-                ) {
-                    treatmentToHighestLevel.put(treament, evidence.getLevelOfEvidence());
+            response
+                .getEvidences()
+                .stream()
+                .filter(evidence -> !evidence.getCancerTypes().isEmpty() && evidence.getCancerTypes().contains(uniqueTumorType))
+                .forEach(evidence -> {
+                    String treatment = TreatmentUtils.getTreatmentName(evidence.getTreatments());       
+                    if (treatmentToHighestLevel.get(treatment) == null 
+                        || LevelUtils.compareLevel(evidence.getLevelOfEvidence(), treatmentToHighestLevel.get(treatment), LevelUtils.THERAPEUTIC_SENSITIVE_LEVELS) < 0
+                    ) {
+                        treatmentToHighestLevel.put(treatment, evidence.getLevelOfEvidence());
+                    }
+                    
+                    Evidence updatedEvidence = new Evidence(evidence, evidence.getId());
+                    if (updatedEvidence.getGene().getHugoSymbol().equals("ESR1")) {
+                        updatedEvidence = MainUtils.convertSpecialESR1Evidence(updatedEvidence);
+                    }
+                    if (updatedEvidence.getRelevantCancerTypes() == null || updatedEvidence.getRelevantCancerTypes().size() == 0) {
+                        updatedEvidence.setRelevantCancerTypes(TumorTypeUtils.findEvidenceRelevantCancerTypes(evidence));
+                    }
+                    updatedEvidences.add(updatedEvidence);
                 }
-                
-                Evidence updatedEvidence = new Evidence(evidence, evidence.getId());
-                if (updatedEvidence.getGene().getHugoSymbol().equals("ESR1")) {
-                    updatedEvidence = MainUtils.convertSpecialESR1Evidence(updatedEvidence);
-                }
-                if (updatedEvidence.getRelevantCancerTypes() == null || updatedEvidence.getRelevantCancerTypes().size() == 0) {
-                    updatedEvidence.setRelevantCancerTypes(TumorTypeUtils.findEvidenceRelevantCancerTypes(evidence));
-                }
-                updatedEvidences.add(updatedEvidence);
-            }
+            );
 
             // If oncogenic mutations exists when there is also a mutation with a higher level for same cancer type and treament, remove oncogenic mutations
             List<Evidence> filteredEvidences = new ArrayList<>();
@@ -467,7 +472,7 @@ public class PrivateUtilsApiController implements PrivateUtilsApi {
                     continue;
                 }
 
-                Alteration alt = (Alteration) evidence.getAlterations().toArray()[0];
+                Alteration alt = (Alteration) evidence.getAlterations().iterator().next();
                 if (!alt.getAlteration().equals(InferredMutation.ONCOGENIC_MUTATIONS.getVariant())) {
                     filteredEvidences.add(evidence);
                     continue;
