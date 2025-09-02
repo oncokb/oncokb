d95e43136a132823b34ca650cdc2692ea5987756
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2025-09-02 16:25:18 -0400

    Fix unit test git action

--- unit_test/scripts/run_docker_test.sh
+++ unit_test/scripts/run_docker_test.sh
@@ -3,4 +3,4 @@
 export MYSQL_ROOT_PASSWORD=rootroot
 
 mvn -ntp package -P enterprise -DskipTests=true
-docker-compose -f unit_test/scripts/docker-compose.yml up --build --exit-code-from maven 
+docker compose -f unit_test/scripts/docker-compose.yml up --build --exit-code-from maven 
