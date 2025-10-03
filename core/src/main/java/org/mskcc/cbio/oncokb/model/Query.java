d4a247c5038e8b5c98e3fd388f78e6222f3eec90
bprize15 <bprize15@gmail.com>
2025-10-03 14:32:41 -0400

    make entrez gene id positive if not other biomarkers

--- core/src/main/java/org/mskcc/cbio/oncokb/model/Query.java
+++ core/src/main/java/org/mskcc/cbio/oncokb/model/Query.java
@@ -2,12 +2,14 @@ package org.mskcc.cbio.oncokb.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mskcc.cbio.oncokb.apiModels.annotation.*;
+import org.mskcc.cbio.oncokb.serializer.EntrezGeneIdConverter;
 import org.mskcc.cbio.oncokb.util.AlterationUtils;
 import org.mskcc.cbio.oncokb.util.AminoAcidConverterUtils;
 import org.mskcc.cbio.oncokb.util.GeneUtils;
@@ -33,6 +35,7 @@ public class Query implements java.io.Serializable {
     private String hugoSymbol;
 
     @ApiModelProperty(value = "(Nullable) Unique gene identifiers from NCBI. May be null if omitted from original query, otherwise filled in by OncoKB")
+    @JsonSerialize(converter = EntrezGeneIdConverter.class)
     private Integer entrezGeneId;
 
     @ApiModelProperty(value = "(Nullable) Alteration from original query or the resolved alteration from HGVS variant")
