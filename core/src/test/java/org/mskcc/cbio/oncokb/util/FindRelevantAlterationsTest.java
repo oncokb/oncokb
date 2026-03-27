cd9f9eceece089241febb948fa345119e06f64f1
bprize15 <bprize15@gmail.com>
2026-03-27 15:49:03 -0400

    update tests for data release 7.0

--- core/src/test/java/org/mskcc/cbio/oncokb/util/FindRelevantAlterationsTest.java
+++ core/src/test/java/org/mskcc/cbio/oncokb/util/FindRelevantAlterationsTest.java
@@ -79,7 +79,7 @@ public class FindRelevantAlterationsTest {
                 {"NF1", "L1340_Q1341delinsF*", null, "1_2772trunc, Oncogenic Mutations"},
 
                 // Check stop_lost, especially it should not be associated with truncating mutation
-                {"FGFR3", "*807Wext*101", null, "*807Wext*101, Oncogenic Mutations"},
+                {"FGFR3", "*807Wext*101", null, "*807Wext*101, *807Rext*141, Oncogenic Mutations"},
 
                 // Check inframe-insertion, inframe-deletion
                 {"EGFR", "Y764_D770dup", null, "Y764_D770dup, 762_823ins, A767_V769dup, S768_D770dup, A767_S768insASV, S768_V769insSVD, S768_V769insVAS, V769_D770insASV, V769_D770insGVV, D770delinsGTH, D770delinsGY, A763_Y764insFQEA, D770_N771insD, D770_N771insG, D770_N771insGF, D770_N771insGL, D770_N771insNPG, D770_N771insSVD, D770_N771insVDSVDNP, D770_N771insY, D770_P772dup, Oncogenic Mutations, 762_823ins {excluding A763_Y764insFQEA}"},
@@ -141,8 +141,7 @@ public class FindRelevantAlterationsTest {
                 // Deletion
                 // With specific Deletion curated
                 {"BRCA2", "Deletion", null, "Deletion, Oncogenic Mutations"},
-                // Without specific Deletion curated
-                {"MAP2K4", "Deletion", null, "Truncating Mutations"},
+                {"MAP2K4", "Deletion", null, "Deletion"},
 
                 // Abbreviated alterations
                 {"EGFR", "KDD", null, "Kinase Domain Duplication, Oncogenic Mutations"},
